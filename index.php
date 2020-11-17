b6ed88e15b96efa3fe7e255f65764ae42cba8bcb
Dyan030 <lin910475@gmail.com>
2020-11-17 16:36:26 +0800

    創index.php
    首頁

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include __DIR__ . '/parts/config.php' ?>
+<?php include __DIR__ . '/parts/html-head.php' ?>
+<!-- --- css 連結放下面 ----- -->
+
+
+<?php include __DIR__ . '/parts/html-navbar.php' ?>
+<!-- ------------------ body開始 以上勿刪 ------------------ -->
+
+<div class="container">
+
+  <h1 class="orange-color t-xxxl bg-color ">Hello!開始開心寫網站</h1>
+  <br>
+  <h6 class="brown-color t-xxl ">毛日FVEF31111235</h6>
+  <br>
+  <h6 class="green-color t-xl">毛日FVEF31111235</h6>
+  <br>
+  <h6 class="danger-color t-l">毛日FVEF31111235</h6>
+  <br>
+  <h6 class="text-color t-m">毛日FVEF31111235</h6>
+  <br>
+  <h6 class="text-color t-s">毛日FVEF31111235</h6>
+  <br>
+  <h6 class="text-color t-xs">毛日FVEF31111235</h6>
+
+  <button class="btn">測試</button>
+
+</div>
+
+
+
+
+
+
+
+
+
+<!-- ------------------ body結束 ------------------ -->
+<?php include __DIR__ . '/parts/html-footer.php' ?>
+<!-- ---------------js/jq 開始 ------------------ -->
+<?php include __DIR__ . '/parts/html-script.php' ?>
+<script>
+  // ------JS開始 以上勿刪-------
+
+
+
+  // ------JS結束 勿刪到-------
+</script>
+<?php include __DIR__ . '/parts/html-foot.php' ?>
