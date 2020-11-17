d30a8043dfe1e9503e7b43e183dbc682b60fbd1e
ching <ching747474@gmail.com>
2020-11-17 10:34:59 +0800

    新增pdo

--- parts/config.php
+++ parts/config.php
@@ -7,4 +7,8 @@ if (!isset($_SESSION)) {
     session_start();
 }
 
-
+$pdo_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8 ",
+];
