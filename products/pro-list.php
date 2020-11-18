b7cc8bdc29b5c16b99c413d9c41dd10fc93f899b
millie0 <ricebug19@gmail.com>
2020-11-18 21:30:38 +0800

    pdimg

--- products/pro-list.php
+++ products/pro-list.php
@@ -12,14 +12,64 @@
     <div class="row title">
         <p class="">熱門行程</p>
     </div>
-    <div class="row hot">
-        <div class="hot-card w-100">
+</div>
+<div class="container-fluid">
+
+    <div class="row hot align-items-center justify-content-center w-100">
+        <a class="arrow al">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28.62 51.56" width="20px">
+                <defs></defs>
+                <g id="圖層_2" data-name="圖層 2">
+                    <g id="圖層_1-2" data-name="圖層 1">
+                        <path class="cls-2" d="M25.12,51.56a3.52,3.52,0,0,1-2.51-1.06L1,28.22a3.5,3.5,0,0,1,0-4.88L22.61,1.06a3.5,3.5,0,0,1,5,4.88L8.38,25.78,27.64,45.62a3.51,3.51,0,0,1-2.52,5.94Z" />
+                    </g>
+                </g>
+            </svg>
+        </a>
+        <div class="hot-card d-flex">
             <div class="hot-pic">
                 <img src="/petliday/products/img/prolist_hot1.jpg" alt="">
             </div>
-            <div class="hot-text">
-
+            <div class="hot-right position-relative">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 508.3 379.52" height="350px" width="476px">
+                    <defs></defs>
+                    <g id="圖層_1" data-name="圖層 1">
+                        <path class="cls-4" d="M50.16.55v0h-.57V16.93C44.06,96.07,0,101.7,0,191.39s44.06,99.18,49.59,175.19v13.06l200.6.45H508.3V.55Z" />
+                    </g>
+                </svg>
+                <div class="hot-text position-absolute p-5">
+                    <div class="tag d-flex">
+                        <p>tag1</p>
+                        <p>tag2</p>
+                    </div>
+                    <p class="hot-title t-xxl">標題標題標題標題標題標標題標題標題標標</p>
+                    <p class="hot-info">
+                        內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文
+                    </p>
+                    <div class="hot-buttom d-flex justify-content-between align-items-center">
+                        <div class="hot-buttom-left d-flex">
+                            <div class="star">
+                                <img src="/Petliday/icon/star-green.png" alt="">
+                            </div>
+                            <p class="rate t-xl text-gray ml-2">4.8</p>
+                            <p class="rate-num"><u>18則評論</u></p>
+                        </div>
+                        <div class="hot-button-right">
+                            <p class="price orange-color t-xxl">$3880</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <a class="arrow ar">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28.62 51.56" width="20px">
+                <defs></defs>
+                <g id="圖層_2" data-name="圖層 2">
+                    <g id="圖層_1-2" data-name="圖層 1">
+                        <path class="cls-2" d="M3.5,51.56A3.5,3.5,0,0,1,1,45.62L20.25,25.78,1,5.94A3.5,3.5,0,0,1,6,1.06L27.64,23.34a3.52,3.52,0,0,1,0,4.88L6,50.5A3.51,3.51,0,0,1,3.5,51.56Z" />
+                    </g>
+                </g>
+            </svg>
+        </a>
     </div>
 </div>
