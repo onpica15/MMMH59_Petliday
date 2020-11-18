16794711501de6f3234dd7d328202cdbaa255d42
millie0 <ricebug19@gmail.com>
2020-11-18 14:27:56 +0800

    start

---
+++ products/pro-list.php
@@ -0,0 +1,9 @@
+<?php include __DIR__ . '/parts/config.php' ?>
+<?php include __DIR__ . '/parts/html-head.php' ?>
+<?php include __DIR__ . '/parts/html-navbar.php' ?>
+
+<div class="container">
+    <div class="row top-pic">
+        <img src="./Petliday/products/img/prolist-top.png" alt="">
+    </div>
+</div>
