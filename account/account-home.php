b1cd7351ff1a7175867e0f052c7a2dc25a2557cb
iiujung <foradadad@gmail.com>
2020-11-19 12:03:36 +0800

    avatar

--- account/account-home.php
+++ account/account-home.php
@@ -7,17 +7,18 @@
 <?php include __DIR__ . '/../parts/html-navbar.php' ?>
 <!-- ------------------ body開始 以上勿刪 ------------------ -->
 <section id="acount">
-    <div class="container">
-        <div class="row">
+    <div class="container ">
+        <div class="row ">
             <div class="col-3">
+                <h2 class="list-group-item">會員中心</h2>
+
                 <div class="list-group" id="list-tab" role="tablist">
-                    <h2 class="list-group-item">會員中心</h2>
 
                     <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">會員資料</a>
 
-                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-wishlist" role="tab" aria-controls="profile">願望清單</a>
-
                     <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-order" role="tab" aria-controls="messages">我的訂單</a>
+
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-wishlist" role="tab" aria-controls="profile">願望清單</a>
 
                     <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-mailbox" role="tab" aria-controls="settings">會員信箱</a>
 
@@ -31,13 +32,14 @@
                     <div class="tab-pane fade show active" id="list-profile" role="tabpanel" aria-labelledby="list-home-list">
                         <div class="col mb-3" id="member-list">
                             <div class="row">
-                                <div class="line">123</div>
+                                <div class="line mr-2"></div>
                                 <h4>會員資料</h4>
                             </div>
                             <div class="row">
                                 <div class="col-4">
                                     <div class="member-img">
-                                        img </div>
+                                        <img src="./img/avatar-pet1.jpg" alt="">
+                                    </div>
                                     <div class="member-id">
                                         ID:petty777
                                     </div>
@@ -71,7 +73,7 @@
                                     </form>
                                 </div>
                             </div>
-                            <div class="row">
+                            <div class="row justify-content-end">
                                 <button>儲存修改</button>
                                 <button>密碼更改</button>
 
@@ -79,13 +81,38 @@
                         </div>
                         <div class="col mb-3" id="pet-list">
                             <div class="row">
+                                <div class="line mr-2"></div>
                                 <h4>寵物資料</h4>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="pet-img">
+                                        <img src="" alt="">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h6>布丁狗</h6>
+                                    <div class="pet-detail">
+                                        <p>品種:</p>
+                                        <p>年齡:</p>
+                                        <p>生日:</p>
+                                    </div>
+
+                                </div>
+                                <div class="col align-self-end">
+                                    <a href="">edit</a>
+                                    <a href="">remove</a>
+                                </div>
+                            </div>
+                            <div class="row justify-content-end">
+                                <button>新增寵物</button>
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane fade" id="list-wishlist" role="tabpanel" aria-labelledby="list-profile-list">list-wishlist</div>
 
                     <div class="tab-pane fade" id="list-order" role="tabpanel" aria-labelledby="list-messages-list">list-order</div>
+
+                    <div class="tab-pane fade" id="list-wishlist" role="tabpanel" aria-labelledby="list-profile-list">list-wishlist</div>
 
                     <div class="tab-pane fade" id="list-mailbox" role="tabpanel" aria-labelledby="list-settings-list">list-mailbox</div>
 
