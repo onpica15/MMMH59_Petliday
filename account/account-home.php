f956bc743252a6aa7f30a7eb5a7d86d94d8858ff
iiujung <foradadad@gmail.com>
2020-11-19 10:48:02 +0800

    account-list

--- account/account-home.php
+++ account/account-home.php
@@ -1,15 +1,102 @@
 <?php include __DIR__ . '/../parts/config.php' ?>
 <?php include __DIR__ . '/../parts/html-head.php' ?>
 <!-- --- css 連結放下面 ----- -->
-<style>
+<link rel="stylesheet" href="style.css">
 
-</style>
 
 <?php include __DIR__ . '/../parts/html-navbar.php' ?>
 <!-- ------------------ body開始 以上勿刪 ------------------ -->
+<section id="acount">
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="list-group" id="list-tab" role="tablist">
+                    <h2 class="list-group-item">會員中心</h2>
 
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">會員資料</a>
 
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-wishlist" role="tab" aria-controls="profile">願望清單</a>
 
+                    <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-order" role="tab" aria-controls="messages">我的訂單</a>
+
+                    <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-mailbox" role="tab" aria-controls="settings">會員信箱</a>
+
+                    <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-coupon" role="tab" aria-controls="settings">優惠券</a>
+
+                    <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-qa" role="tab" aria-controls="settings">Q&A</a>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="list-profile" role="tabpanel" aria-labelledby="list-home-list">
+                        <div class="col mb-3" id="member-list">
+                            <div class="row">
+                                <div class="line">123</div>
+                                <h4>會員資料</h4>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="member-img">
+                                        img </div>
+                                    <div class="member-id">
+                                        ID:petty777
+                                    </div>
+                                </div>
+                                <div class="col-8">
+                                    <form>
+                                        <div class="form-group row">
+                                            <label for="staticEmail" class="col-sm-2 col-form-label">姓名</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="霈霈">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="staticEmail" class="col-sm-2 col-form-label">生日</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="1996/09/10">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="pitypetty12@example.com">
+                                            </div>
+                                        </div>
+                                        <!-- <div class="form-group row">
+                                            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                                            <div class="col-sm-10">
+                                                <input type="password" class="form-control" id="inputPassword">
+                                            </div>
+                                        </div> -->
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <button>儲存修改</button>
+                                <button>密碼更改</button>
+
+                            </div>
+                        </div>
+                        <div class="col mb-3" id="pet-list">
+                            <div class="row">
+                                <h4>寵物資料</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="list-wishlist" role="tabpanel" aria-labelledby="list-profile-list">list-wishlist</div>
+
+                    <div class="tab-pane fade" id="list-order" role="tabpanel" aria-labelledby="list-messages-list">list-order</div>
+
+                    <div class="tab-pane fade" id="list-mailbox" role="tabpanel" aria-labelledby="list-settings-list">list-mailbox</div>
+
+                    <div class="tab-pane fade" id="list-coupon" role="tabpanel" aria-labelledby="list-settings-list">list-coupon</div>
+
+                    <div class="tab-pane fade" id="list-qa" role="tabpanel" aria-labelledby="list-settings-list">list-coupon</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
