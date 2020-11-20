8e7b72d03759e39be035f5f94156b644bcbad815
Dyan030 <lin910475@gmail.com>
2020-11-20 12:46:26 +0800

    看廠商談戀愛時更新ㄉ

--- index/index＿.php
+++ index/index＿.php
@@ -8,107 +8,114 @@
 <!-- 拍立得section -->
 <section id="hero-polaroid">
   <div class="polaroid-base d-flex position-relative justify-content-center">
-    <div class="polaroid-figure col-10 position-absolute"></div>
+    <figure class="polaroid-figure col-11 position-absolute">
+    </figure>
+    <figure class="signature position-absolute">
+      <img src="./img/signature1.png" alt="">
+    </figure>
   </div>
 </section>
-<!-- 插圖介紹section -->
-<section id="petliday-wrap">
-  <div class="container">
-    <div class="row justify-item-center my-5">
-      <div class="logo-sentence mx-auto">
-        <img src="img/illustration/petliday.png" alt="">
+
+<section id="bg1">
+  <!-- 插圖介紹section -->
+  <section id="petliday-wrap">
+    <div class="container">
+      <div class="row justify-item-center mb-5">
+        <div class="logo-sentence mx-auto">
+          <img src="img/illustration/petliday.png" alt="">
+        </div>
+      </div>
+      <div class="row d-flex justify-content-between my-5">
+        <div class="col-4 d-flex petliday-card">
+          <figure class="row petliday-fig mx-auto">
+            <img src="./img/illustration/index-dog1.png" alt="">
+          </figure>
+          <div class="row">
+            <p class="petliday-title">放鬆</p>
+            <p class="petliday-content">最多樣的行程安排，讓飼主可以和寶貝毛孩一起在旅行中放鬆</p>
+          </div>
+        </div>
+
+        <div class="col-4 d-flex petliday-card">
+          <figure class="row petliday-fig mx-auto">
+            <img src="img/illustration/index-dog2.png" alt="">
+          </figure>
+          <div class="row">
+            <p class="petliday-title">一起體驗</p>
+            <p class="petliday-content">我們搜羅了全台最適合與狗狗一起同遊、體驗的景點與活動！</p>
+          </div>
+        </div>
+
+        <div class="col-4 d-flex petliday-card">
+          <figure class="row petliday-fig mx-auto">
+            <img src="img/illustration/index-dog3.png" alt="">
+          </figure>
+          <div class="row">
+            <p class="petliday-title">放鬆</p>
+            <p class="petliday-content">最多樣的行程安排，讓飼主可以和寶貝毛孩一起在旅行中放鬆</p>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="row d-flex justify-content-between my-5">
-      <div class="col-4 d-flex petliday-card">
-        <figure class="row petliday-fig mx-auto">
-          <img src="img/illustration/index-dog1.png" alt="">
-        </figure>
-        <div class="row">
-          <p class="petliday-title">放鬆</p>
-          <p class="petliday-content">最多樣的行程安排，讓飼主可以和寶貝毛孩一起在旅行中放鬆</p>
-        </div>
-      </div>
-
-      <div class="col-4 d-flex petliday-card">
-        <figure class="row petliday-fig mx-auto">
-          <img src="img/illustration/index-dog2.png" alt="">
-        </figure>
-        <div class="row">
-          <p class="petliday-title">一起體驗</p>
-          <p class="petliday-content">我們搜羅了全台最適合與狗狗一起同遊、體驗的景點與活動！</p>
-        </div>
-      </div>
-
-      <div class="col-4 d-flex petliday-card">
-        <figure class="row petliday-fig mx-auto">
-          <img src="img/illustration/index-dog3.png" alt="">
-        </figure>
-        <div class="row">
-          <p class="petliday-title">放鬆</p>
-          <p class="petliday-content">最多樣的行程安排，讓飼主可以和寶貝毛孩一起在旅行中放鬆</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- 同樂目的地section -->
-<section id="destination">
-  <!-- 腳印標題那部分 -->
-  <div class="container">
-    <div class="row d-flex flex-start align-items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
-        <g id="圖層_2" data-name="圖層 2">
-          <g id="圖層_1-2" data-name="圖層 1">
-            <g id="Group_518" data-name="Group 518">
-              <path id="Path_424" data-name="Path 424" class="cls-1" d="M19,10.48a10.48,10.48,0,0,0-12.88,0,6.6,6.6,0,0,0-2.8,5c0,3.27,3.54,5.3,9.24,5.3s9.24-2,9.24-5.3A6.6,6.6,0,0,0,19,10.48Z" />
-              <path id="Path_425" data-name="Path 425" class="cls-1" d="M12.61,6.91A3.47,3.47,0,1,0,8.75,3.47,3.68,3.68,0,0,0,12.61,6.91Z" />
-              <path id="Path_426" data-name="Path 426" class="cls-1" d="M25,5.85a4.26,4.26,0,0,0-4.61-2.66,3.15,3.15,0,0,0-2.66,3.55,3,3,0,0,0,.2.72A4.13,4.13,0,0,0,19.74,9.6a4.34,4.34,0,0,0,2.11.58,3.68,3.68,0,0,0,.66-.06,3.12,3.12,0,0,0,2.66-3.55A3.06,3.06,0,0,0,25,5.85Z" />
-              <path id="Path_427" data-name="Path 427" class="cls-1" d="M7.25,7.46A3.12,3.12,0,0,0,5.51,3.39a3.09,3.09,0,0,0-.72-.2A4.12,4.12,0,0,0,2,3.72,3.65,3.65,0,0,0,.37,8.59h0a3.25,3.25,0,0,0,2.27,1.53,3.68,3.68,0,0,0,.66.06A4.14,4.14,0,0,0,5.41,9.6,4.07,4.07,0,0,0,7.25,7.46Z" />
+  </section>
+  <!-- 同樂目的地section -->
+  <section id="destination">
+    <!-- 腳印標題那部分 -->
+    <div class="container my-5">
+      <div class="row d-flex flex-start align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
+          <g id="圖層_2" data-name="圖層 2">
+            <g id="圖層_1-2" data-name="圖層 1">
+              <g id="Group_518" data-name="Group 518">
+                <path id="Path_424" data-name="Path 424" class="cls-1" d="M19,10.48a10.48,10.48,0,0,0-12.88,0,6.6,6.6,0,0,0-2.8,5c0,3.27,3.54,5.3,9.24,5.3s9.24-2,9.24-5.3A6.6,6.6,0,0,0,19,10.48Z" />
+                <path id="Path_425" data-name="Path 425" class="cls-1" d="M12.61,6.91A3.47,3.47,0,1,0,8.75,3.47,3.68,3.68,0,0,0,12.61,6.91Z" />
+                <path id="Path_426" data-name="Path 426" class="cls-1" d="M25,5.85a4.26,4.26,0,0,0-4.61-2.66,3.15,3.15,0,0,0-2.66,3.55,3,3,0,0,0,.2.72A4.13,4.13,0,0,0,19.74,9.6a4.34,4.34,0,0,0,2.11.58,3.68,3.68,0,0,0,.66-.06,3.12,3.12,0,0,0,2.66-3.55A3.06,3.06,0,0,0,25,5.85Z" />
+                <path id="Path_427" data-name="Path 427" class="cls-1" d="M7.25,7.46A3.12,3.12,0,0,0,5.51,3.39a3.09,3.09,0,0,0-.72-.2A4.12,4.12,0,0,0,2,3.72,3.65,3.65,0,0,0,.37,8.59h0a3.25,3.25,0,0,0,2.27,1.53,3.68,3.68,0,0,0,.66.06A4.14,4.14,0,0,0,5.41,9.6,4.07,4.07,0,0,0,7.25,7.46Z" />
+              </g>
             </g>
           </g>
-        </g>
-      </svg>
-      <span>
-        <h6 class="des-title brown-color t-xxl m-0">同樂目的地</h6>
-      </span>
-    </div>
-  </div>
-  <!-- 同樂目的地卡片 -->
-  <div class="container">
-    <div class="row d-flex des-row flex-nowrap justify-content-center">
-      <div class="col-3 des-card">
-        <figure>
-          <img src="img/scenary-im2.jpg" alt="">
-        </figure>
-        <h6 class="t-l">北部旅遊</h6>
-      </div>
-      <div class="col-3 des-card">
-        <figure>
-          <img src="" alt="">
-        </figure>
-        <h6 class="t-l">中部旅遊</h6>
-      </div>
-      <div class="col-3 des-card">
-        <figure>
-          <img src="" alt="">
-        </figure>
-        <h6 class="t-l">南部旅遊</h6>
-      </div>
-      <div class="col-3 des-card">
-        <figure>
-          <img src="" alt="">
-        </figure>
-        <h6 class="des-card-text t-l">東部、離島旅遊</h6>
+        </svg>
+        <span>
+          <h6 class="des-title brown-color t-xxl m-0">同樂目的地</h6>
+        </span>
       </div>
     </div>
-  </div>
-  </div>
+    <!-- 同樂目的地卡片 -->
+    <div class="container">
+      <div class="row d-flex des-row flex-nowrap justify-content-center">
+        <div class="col-3 des-card">
+          <figure class="mt-4">
+            <img src="./img/scenery-im2.jpg" alt="">
+          </figure>
+          <h6 class="green-color t-l text-center">北部旅遊</h6>
+        </div>
+        <div class="col-3 des-card">
+          <figure class="mt-4">
+            <img src="./img/scenery-im1.jpg" alt="">
+          </figure>
+          <h6 class="green-color t-l text-center">中部旅遊</h6>
+        </div>
+        <div class="col-3 des-card">
+          <figure class="mt-4">
+            <img src="./img/scenery-im4.jpg" alt="">
+          </figure>
+          <h6 class="green-color t-l text-center">南部旅遊</h6>
+        </div>
+        <div class="col-3 des-card">
+          <figure class="mt-4">
+            <img src="./img/scenery-im3.jpg" alt="">
+          </figure>
+          <h6 class="des-card-text green-color t-l text-center">東部、離島旅遊</h6>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
 </section>
 <!-- 精選行程section -->
 <section id="tour">
   <!-- 腳印標題那部分 -->
-  <div class="container">
+  <div class="container my-5">
     <div class="row d-flex flex-start align-items-center">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
         <g id="圖層_2" data-name="圖層 2">
@@ -132,7 +139,7 @@
     <div class="row d-flex tour-row flex-nowrap justify-content-center">
       <div class="col-4 tour-card">
         <figure>
-          <img src="" alt="">
+          <img src="./img/pd2.jpg" alt="">
         </figure>
         <h6 class="t-l">聖誕節精選行程一芭芭拉拉巴拉巴</h6>
         <!-- 星等、評價、價錢 -->
@@ -159,7 +166,7 @@
       </div>
       <div class="col-4 tour-card">
         <figure>
-          <img src="" alt="">
+          <img src="./img/pd6.jpg" alt="">
         </figure>
         <h6 class="t-l">聖誕節精選行程二芭芭拉拉巴拉巴</h6>
         <!-- 星等、評價、價錢 -->
@@ -186,7 +193,7 @@
       </div>
       <div class="col-4 tour-card">
         <figure>
-          <img src="" alt="">
+          <img src="./img/pd10.jpg" alt="">
         </figure>
         <h6 class="t-l">聖誕節精選行程三芭芭拉拉巴拉巴</h6>
         <!-- 星等、評價、價錢 -->
@@ -219,7 +226,7 @@
 <!-- 體驗課程section -->
 <section id="class">
   <!-- 腳印標題那部分 -->
-  <div class="container">
+  <div class="container my-5">
     <div class="row d-flex flex-start align-items-center">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
         <g id="圖層_2" data-name="圖層 2">
@@ -468,8 +475,23 @@
   </div>
   <div class="container my-5">
     <div class="row d-flex justify-content-between">
+      <figure class="chatpic-wrap col-6 position-relative">
+        <img src="./img/depth-chat.png" alt="" style="transform: scaleX(-1)">
+        <div class="text-wrap2 position-absolute">
+          <h6 class="brown-color t-l">體驗深度旅遊</h6>
+          <h6 class="brown-color t-m">人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊</h6>
+          <button class="btn m-0 float-right">立即訂購</button>
+        </div>
+      </figure>
       <figure class="col-6 mb-0">
-        <img src="./img/dog1.png" alt="">
+        <img src="./img/dog2.png" alt="">
+      </figure>
+    </div>
+  </div>
+  <div class="container my-5">
+    <div class="row d-flex justify-content-between">
+      <figure class="col-6 mb-0">
+        <img src="./img/dog3.png" alt="">
       </figure>
       <figure class="chatpic-wrap col-6 position-relative">
         <img src="./img/depth-chat.png" alt="">
@@ -481,20 +503,63 @@
       </figure>
     </div>
   </div>
+
+</section>
+<!-- 小測驗section -->
+<section id="quiz">
+  <!-- 腳印標題那部分 -->
   <div class="container my-5">
-    <div class="row d-flex justify-content-between">
-      <figure class="col-6 mb-0">
-        <img src="./img/dog1.png" alt="">
-      </figure>
-      <figure class="chatpic-wrap col-6 position-relative">
-        <img src="./img/depth-chat.png" alt="">
-        <div class="text-wrap position-absolute">
-          <h6 class="brown-color t-l">體驗深度旅遊</h6>
-          <h6 class="brown-color t-m">人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊的重要性。既然如此，了解清楚旅遊到底人生。想必大家都能了解旅遊</h6>
-          <button class="btn m-0 float-right">立即訂購</button>
-        </div>
-      </figure>
+    <div class="row d-flex flex-start align-items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
+        <g id="圖層_2" data-name="圖層 2">
+          <g id="圖層_1-2" data-name="圖層 1">
+            <g id="Group_518" data-name="Group 518">
+              <path id="Path_424" data-name="Path 424" class="cls-1" d="M19,10.48a10.48,10.48,0,0,0-12.88,0,6.6,6.6,0,0,0-2.8,5c0,3.27,3.54,5.3,9.24,5.3s9.24-2,9.24-5.3A6.6,6.6,0,0,0,19,10.48Z" />
+              <path id="Path_425" data-name="Path 425" class="cls-1" d="M12.61,6.91A3.47,3.47,0,1,0,8.75,3.47,3.68,3.68,0,0,0,12.61,6.91Z" />
+              <path id="Path_426" data-name="Path 426" class="cls-1" d="M25,5.85a4.26,4.26,0,0,0-4.61-2.66,3.15,3.15,0,0,0-2.66,3.55,3,3,0,0,0,.2.72A4.13,4.13,0,0,0,19.74,9.6a4.34,4.34,0,0,0,2.11.58,3.68,3.68,0,0,0,.66-.06,3.12,3.12,0,0,0,2.66-3.55A3.06,3.06,0,0,0,25,5.85Z" />
+              <path id="Path_427" data-name="Path 427" class="cls-1" d="M7.25,7.46A3.12,3.12,0,0,0,5.51,3.39a3.09,3.09,0,0,0-.72-.2A4.12,4.12,0,0,0,2,3.72,3.65,3.65,0,0,0,.37,8.59h0a3.25,3.25,0,0,0,2.27,1.53,3.68,3.68,0,0,0,.66.06A4.14,4.14,0,0,0,5.41,9.6,4.07,4.07,0,0,0,7.25,7.46Z" />
+            </g>
+          </g>
+        </g>
+      </svg>
+      <span>
+        <h6 class="des-title brown-color t-xxl m-0">測驗最適合您的行程</h6>
+      </span>
     </div>
+  </div>
+  <!-- 選項＋卡片 -->
+  <div class="container">
+    <div class="row justify-content-between">
+      <div class="col-3 m-0">
+        <figure>
+          <img class="opt" src="./img/quest-img1.jpg" alt="">
+        </figure>
+      </div>
+      <div class="col-6 m-0 d-flex align-items-center">
+        <div class="box">
+          <div class="text-wrap d-flex flex-column w-100">
+            <h6 class="green-color t-l">Q2. 請問毛孩較為喜歡靜態活動還是動態活動呢？</h6>
+            <h6 class="t-m">A. 喜歡待在家自己找東西玩，或和熟悉的人玩樂<br>B. 喜歡在外面奔跑，對其他人狗不怕生
+            </h6>
+          </div>
+        </div>
+      </div>
+      <div class="col-3 m-0 ">
+        <figure>
+          <img class="opt" src="./img/quest-img2.jpg" alt="">
+        </figure>
+      </div>
+    </div>
+    <div class="row justify-content-between">
+      <h6 class="col-3 t-xxl option green-color">A.</h6>
+      <h6 class="col-3 t-xxl option green-color">B.</h6>
+    </div>
+
+    <div>
+    </div>
+    <figure class="tutor col-1">
+      <img src="./img/illustration/mouse-react.png" alt=""></figure>
+    <h6 class="text-gray t-xs text-center mx-auto mt-2 col-3">請將卡片拖曳至您的答案</h6>
   </div>
 
 </section>
