9c39becc5620ee043384849f8bf1472c6357c657
YuChieh <la0603c@gmail.com>
2020-11-20 10:53:19 +0800

    login 改成手刻版

--- userlogin/user-login.php
+++ userlogin/user-login.php
@@ -5,72 +5,60 @@
 
 <?php include __DIR__ . '/../parts/html-navbar.php' ?>
 <!-- ------------------ body開始 以上勿刪 ------------------ -->
+<div class="container mt-5">
+
+  <h1 class="orange-color t-xxxl bg-color ">Hello!開始開心寫網站 orange-color t-xxxl bg-color</h1>
+  <br>
+  <h6 class="brown-color t-xxl ">毛日 brown-color t-xxl</h6>
+  <br>
+  <h6 class="green-color t-xl">毛日 green-color t-xl</h6>
+  <br>
+  <h6 class="danger-color t-l">毛日 danger-color t-l</h6>
+  <br>
+  <h6 class="text-color t-m">毛日 text-color t-m</h6>
+  <br>
+  <h6 class="text-color t-s">毛日 text-color t-s</h6>
+  <br>
+  <h6 class="text-gray t-xs">毛日 text-gray t-xs</h6>
+
+  <button class="btn">測試 class='btn'</button>
+
+</div>
 
 <div class="container">
   <div class="row">
+    <div class="login">
+      <form class="login-form">
+        <h2 class="t-xl">使用社群平台帳戶登入</h2>
+        <h2 class="t-m">立即登入，隨時給毛孩獨家優惠</h2>
+        <div class="login-social">
+          <!-- 放社群icon -->
+        </div>
+        
+        <div class="col-lg-5" style="padding-left: 0;">
+          <hr>
+        </div>
+        <div class="col-lg-2" style="font-size: 14px; text-align: center; padding: 7px;">
+          或
+        </div>
+        <div class="col-lg-5" style="padding-right: 0;">
+          <hr>
+        </div>
 
-
-  <!-- 舊版 -->
-    <!-- <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content"> -->
-
-          <!-- <div class="modal-header">
-            <h5 class="modal-title t-l" id="exampleModalCenterTitle">使用社群平台帳戶登入</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div> -->
-
-
-          <!-- <div class="modal-body">
-            <form>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --> 
-              <!-- </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-              </div>
-              <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-              </div>
-              <button type="submit" class="btn">Submit</button>
-            </form>
-          </div> --> 
-
-          <!-- <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div> -->
-        <!-- </div>
-      </div>
-    </div> -->
-
-    <div>
-      <h1 class="orange-color t-xxxl bg-color ">Hello!開始開心寫網站 orange-color t-xxxl bg-color</h1>
-      <br>
-      <h6 class="brown-color t-xxl ">毛日 brown-color t-xxl</h6>
-      <br>
-      <h6 class="green-color t-xl">毛日 green-color t-xl</h6>
-      <br>
-      <h6 class="danger-color t-l">毛日 danger-color t-l</h6>
-      <br>
-      <h6 class="text-color t-m">毛日 text-color t-m</h6>
-      <br>
-      <h6 class="text-color t-s">毛日 text-color t-s</h6>
-      <br>
-      <h6 class="text-gray t-xs">毛日 text-gray t-xs</h6>
-
-      <button class="btn">測試 class='btn'</button>
+        <h2 class="t-xl">Petliday會員登入</h2>
+        <div class="login-group">
+          <!-- <label for="account">電子信箱</label> -->
+          <input type="text" class="form-control" id="account" name="account" placeholder="電子信箱">
+        </div>
+        <div class="login-group">
+          <!-- <label for="password">密碼</label> -->
+          <input type="password" class="form-control" id="password" name="password" placeholder="密碼">
+        </div>
+      </form>
     </div>
   </div>
 </div>
 
-<dialog></dialog>
 
 
 <!-- 範例 -->
