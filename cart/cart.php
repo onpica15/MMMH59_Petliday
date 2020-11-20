b18e53bdc4361b58037df5166f6ecb63860158ee
ching <ching747474@gmail.com>
2020-11-20 15:07:47 +0800

    nav+footer

--- cart/cart.php
+++ cart/cart.php
@@ -18,7 +18,7 @@
   <div class="row">
     <div class="col-12 prod-box p-4 d-flex">
       <div class="prod-img ">
-        <img src="../products/img/pdpage-info-img2.jpg" alt="">
+        <img src="../products/img/pd6.jpg" alt="">
       </div>
       <div class="prod-text pl-3 pr-4 d-flex flex-column align-content-between">
         <h5 class="brown-color t-l">標題標題標題標題標題標題標題標題題標題</h5>
@@ -55,7 +55,126 @@
       </div>
 
     </div>
+    <div class="col-12 prod-box p-4 d-flex">
+      <div class="prod-img ">
+        <img src="../products/img/pd6.jpg" alt="">
+      </div>
+      <div class="prod-text pl-3 pr-4 d-flex flex-column align-content-between">
+        <h5 class="brown-color t-l">標題標題標題標題標題標題標題標題題標題</h5>
+        <div class="small-text">
+          <h6 class=" text-color t-s">2020/08/12 pm 6:00</h6>
+          <p class=" green-color t-s">修改日期 ></p>
+        </div>
+
+      </div>
+      <div class="change-box align-items-center">
+        <div class="man-box p-2">
+
+          <div class="change-man d-flex d-flex align-items-center">
+            <p class="mr-1 mb-0">人數：</p>
+            <div class="change-btn subtr-btn">-</div>
+            <div class="change-quantity">1</div>
+            <div class="change-btn add-btn">+</div>
+          </div>
+        </div>
+        <div class="pet-box p-2">
+
+          <div class="change-man d-flex d-flex align-items-center">
+            <p class="mr-1 mb-0">寵物：</p>
+            <div class="change-btn subtr-btn">-</div>
+            <div class="change-quantity">1</div>
+            <div class="change-btn add-btn">+</div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="prod-price p-5 d-flex align-items-center">
+        <h5 class="t-m brown-color">NT$ 3000</h5>
+      </div>
+
+    </div>
+    <div class="col-12 prod-box p-4 d-flex">
+      <div class="prod-img ">
+        <img src="../products/img/pd6.jpg" alt="">
+      </div>
+      <div class="prod-text pl-3 pr-4 d-flex flex-column align-content-between">
+        <h5 class="brown-color t-l">標題標題標題標題標題標題標題標題題標題</h5>
+        <div class="small-text">
+          <h6 class=" text-color t-s">2020/08/12 pm 6:00</h6>
+          <p class=" green-color t-s">修改日期 ></p>
+        </div>
+
+      </div>
+      <div class="change-box align-items-center">
+        <div class="man-box p-2">
+
+          <div class="change-man d-flex d-flex align-items-center">
+            <p class="mr-1 mb-0">人數：</p>
+            <div class="change-btn subtr-btn">-</div>
+            <div class="change-quantity">1</div>
+            <div class="change-btn add-btn">+</div>
+          </div>
+        </div>
+        <div class="pet-box p-2">
+
+          <div class="change-man d-flex d-flex align-items-center">
+            <p class="mr-1 mb-0">寵物：</p>
+            <div class="change-btn subtr-btn">-</div>
+            <div class="change-quantity">1</div>
+            <div class="change-btn add-btn">+</div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="prod-price p-5 d-flex align-items-center">
+        <h5 class="t-m brown-color">NT$ 3000</h5>
+      </div>
+
+    </div>
+    <div class="col-12 prod-box p-4 d-flex">
+      <div class="prod-img ">
+        <img src="../products/img/pd6.jpg" alt="">
+      </div>
+      <div class="prod-text pl-3 pr-4 d-flex flex-column align-content-between">
+        <h5 class="brown-color t-l">標題標題標題標題標題標題標題標題題標題</h5>
+        <div class="small-text">
+          <h6 class=" text-color t-s">2020/08/12 pm 6:00</h6>
+          <p class=" green-color t-s">修改日期 ></p>
+        </div>
+
+      </div>
+      <div class="change-box align-items-center">
+        <div class="man-box p-2">
+
+          <div class="change-man d-flex d-flex align-items-center">
+            <p class="mr-1 mb-0">人數：</p>
+            <div class="change-btn subtr-btn">-</div>
+            <div class="change-quantity">1</div>
+            <div class="change-btn add-btn">+</div>
+          </div>
+        </div>
+        <div class="pet-box p-2">
+
+          <div class="change-man d-flex d-flex align-items-center">
+            <p class="mr-1 mb-0">寵物：</p>
+            <div class="change-btn subtr-btn">-</div>
+            <div class="change-quantity">1</div>
+            <div class="change-btn add-btn">+</div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="prod-price p-5 d-flex align-items-center">
+        <h5 class="t-m brown-color">NT$ 3000</h5>
+      </div>
+
+    </div>
+
   </div>
+
 </div>
 
 <div class="container">
@@ -69,11 +188,12 @@
 
 
     </div>
+
   </div>
 </div>
 </div>
 
-<div class="container mt-5">
+<!-- <div class="container mt-5">
   <h6 class="brown-color t-xxl ">毛日 brown-color t-xxl</h6>
   <br>
   <h6 class="green-color t-xl">毛日 green-color t-xl</h6>
@@ -88,7 +208,7 @@
 
   <button class="btn">測試 class='btn'</button>
 
-</div>
+</div> -->
 
 
 
@@ -108,7 +228,13 @@
 <script>
   // ------JS開始 以上勿刪-------
 
+  // $('.search-box').on('mouseenter', 'mousedown', function() {
+  //   console.log('enter')
+  //   $('.search-bar').addClass('search-hover');
+  //   $('.search-btn').addClass('search-hover');
+  //   $('.search-box button').addClass('search-hover');
 
+  // });
 
   // ------JS結束 勿刪到-------
 </script>
--- parts/html-footer.php
+++ parts/html-footer.php
@@ -1,6 +1,34 @@
 <footer>
-  <div class="container mt-5">
-    <p>footer 測試 </p>
+  <div class="footer-box">
+    <div class="container ">
+      <div class="row">
+        <div class="col-12 social">
+          <div class="social-media d-flex">
+            <div class="social-icon fb-icon mr-3"></div>
+            <div class="social-icon fb-icon mr-3"></div>
+            <div class="social-icon fb-icon mr-3"></div>
+            <div class="social-icon fb-icon"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <small class=" col-12 col-lg-4 company">
+          © 2015-2020 Petliday. Petliday .<br>
+          公司名稱：毛日旅行社有限公司<br>
+          旅行業註冊編號：交觀甲第88888號<br>
+          品保協會會員編號：北88888號<br>
+          地址：臺北市大安區信義路三段<br>
+          電話：02-171788888<br>
+          統一編號：171788888<br>
+          代表人姓名：毛日
+
+
+        </small>
+      </div>
+
+
+
+    </div>
   </div>
 
 </footer>
