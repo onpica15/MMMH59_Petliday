83da127aa71a52d2fa6dcba9a42a7b58f5118c7e
Dyan030 <lin910475@gmail.com>
2020-11-25 00:55:58 +0800

    about

---
+++ about/about.php
@@ -0,0 +1,194 @@
+<?php include __DIR__ . '/../parts/config.php' ?>
+<?php include __DIR__ . '/../parts/html-head.php' ?>
+<!-- --- css 連結放下面 ----- -->
+<link rel="stylesheet" href="about.css">
+
+<?php include __DIR__ . '/../parts/html-navbar.php' ?>
+<!-- ------------------ body開始 以上勿刪 ------------------ -->
+<!-- 照片的部分始 -->
+<section id="hero-polaroid">
+    <div class="container">
+        <div class="row">
+            <div class="pol-wrap position-relative">
+                <div class="box1 col-12 position-absolute"></div>
+                <div class="box2 col-12 position-absolute"></div>
+                <div class="box3 col-12 position-absolute"></div>
+            </div>
+            <div class="polaroid-base d-flex position-relative justify-content-center">
+                <figure class="polaroid-figure col-11 position-absolute">
+                </figure>
+                <figure class="signature position-absolute">
+                    <img src="../index/img/signature1.png" alt="">
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- 照片的部分末 -->
+<!-- 關於毛日的部分始 -->
+<section id="text-wrap">
+    <!-- 腳印的部分 -->
+    <div class="container footprint">
+        <div class="row svg-row d-flex flex-start align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
+                <g id="圖層_2" data-name="圖層 2">
+                    <g id="圖層_1-2" data-name="圖層 1">
+                        <g id="Group_518" data-name="Group 518">
+                            <path id="Path_424" data-name="Path 424" class="cls-1" d="M19,10.48a10.48,10.48,0,0,0-12.88,0,6.6,6.6,0,0,0-2.8,5c0,3.27,3.54,5.3,9.24,5.3s9.24-2,9.24-5.3A6.6,6.6,0,0,0,19,10.48Z" />
+                            <path id="Path_425" data-name="Path 425" class="cls-1" d="M12.61,6.91A3.47,3.47,0,1,0,8.75,3.47,3.68,3.68,0,0,0,12.61,6.91Z" />
+                            <path id="Path_426" data-name="Path 426" class="cls-1" d="M25,5.85a4.26,4.26,0,0,0-4.61-2.66,3.15,3.15,0,0,0-2.66,3.55,3,3,0,0,0,.2.72A4.13,4.13,0,0,0,19.74,9.6a4.34,4.34,0,0,0,2.11.58,3.68,3.68,0,0,0,.66-.06,3.12,3.12,0,0,0,2.66-3.55A3.06,3.06,0,0,0,25,5.85Z" />
+                            <path id="Path_427" data-name="Path 427" class="cls-1" d="M7.25,7.46A3.12,3.12,0,0,0,5.51,3.39a3.09,3.09,0,0,0-.72-.2A4.12,4.12,0,0,0,2,3.72,3.65,3.65,0,0,0,.37,8.59h0a3.25,3.25,0,0,0,2.27,1.53,3.68,3.68,0,0,0,.66.06A4.14,4.14,0,0,0,5.41,9.6,4.07,4.07,0,0,0,7.25,7.46Z" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+            <span>
+                <h6 class="des-title brown-color t-xxl m-0">關於毛日</h6>
+            </span>
+        </div>
+    </div>
+    <!-- 文字的部分 -->
+    <div class="container">
+        <div class="row">
+            <p>我們由一群喜愛自己寵物的人組成，並有著各自擅長的專業領域，過去常常想帶著自己的寵物一起出去玩，卻苦於各大景點及大眾運輸的種種限制，只能含淚將狗狗留在家裡或寄宿在寵物旅館，所以才決定一起提供專門提供狗狗與飼主同遊的旅行社，blahblah。含淚將狗狗留在家裡或寄宿在寵物旅館，所以才決定一起提供專們提供狗狗與飼主同遊的旅行社，blahblah。含淚將狗狗留在家裡或寄宿在寵物旅館，所以才決定一起提供專們提供狗狗與飼主同遊的旅行社，blahblah。</p>
+        </div>
+    </div>
+</section>
+<!-- 關於毛日的部分末 -->
+<!-- 毛日里程碑的部分始 -->
+<section id="milestone">
+    <!-- 腳印的部分 -->
+    <div class="container footprint">
+        <div class="row svg-row d-flex flex-start align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76">
+                <g id="圖層_2" data-name="圖層 2">
+                    <g id="圖層_1-2" data-name="圖層 1">
+                        <g id="Group_518" data-name="Group 518">
+                            <path id="Path_424" data-name="Path 424" class="cls-1" d="M19,10.48a10.48,10.48,0,0,0-12.88,0,6.6,6.6,0,0,0-2.8,5c0,3.27,3.54,5.3,9.24,5.3s9.24-2,9.24-5.3A6.6,6.6,0,0,0,19,10.48Z" />
+                            <path id="Path_425" data-name="Path 425" class="cls-1" d="M12.61,6.91A3.47,3.47,0,1,0,8.75,3.47,3.68,3.68,0,0,0,12.61,6.91Z" />
+                            <path id="Path_426" data-name="Path 426" class="cls-1" d="M25,5.85a4.26,4.26,0,0,0-4.61-2.66,3.15,3.15,0,0,0-2.66,3.55,3,3,0,0,0,.2.72A4.13,4.13,0,0,0,19.74,9.6a4.34,4.34,0,0,0,2.11.58,3.68,3.68,0,0,0,.66-.06,3.12,3.12,0,0,0,2.66-3.55A3.06,3.06,0,0,0,25,5.85Z" />
+                            <path id="Path_427" data-name="Path 427" class="cls-1" d="M7.25,7.46A3.12,3.12,0,0,0,5.51,3.39a3.09,3.09,0,0,0-.72-.2A4.12,4.12,0,0,0,2,3.72,3.65,3.65,0,0,0,.37,8.59h0a3.25,3.25,0,0,0,2.27,1.53,3.68,3.68,0,0,0,.66.06A4.14,4.14,0,0,0,5.41,9.6,4.07,4.07,0,0,0,7.25,7.46Z" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+            <span>
+                <h6 class="des-title brown-color t-xxl m-0">毛日里程碑</h6>
+            </span>
+        </div>
+    </div>
+    <!-- timeline -->
+    <div class="container timeline">
+        <div class="row position-relative">
+            <figure class="col-12">
+                <img src="../index/img/illustration/milestone.png" alt="">
+            </figure>
+            <div class="detail-wrap position-absolute">
+                <p>於課程中塑造了Petliday的雛形</p>
+                <span>
+                    <p>2017.04.12</p>
+                </span>
+            </div>
+            <div class="detail-wrap position-absolute">
+                <p>於信義路三段的北農大樓共同創立了Petliday</p>
+                <span>
+                    <p>2018.06.12</p>
+                </span>
+            </div>
+            <div class="detail-wrap position-absolute">
+                <span>
+                    <p>2018.07.12</p>
+                </span>
+                <p>老闆新增了下午茶吃到飽的福利</p>
+            </div>
+            <div class="detail-wrap position-absolute">
+                <span>
+                    <p>2019.11.24</p>
+                </span>
+                <p>因消費者熱烈回饋，推出短期課程</p>
+            </div>
+            <div class="detail-wrap position-absolute">
+                <p>三週年遇上疫情年，大幅與國內景點合作</p>
+                <span>
+                    <p>2020.12.4</p>
+                </span>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- 毛日里程碑的部分末 -->
+<!-- Petliday是什麼的部分始 -->
+<section id="watis">
+    <!-- 腳印的部分 -->
+    <div class="container footprint">
+        <div class="row svg-row d-flex flex-start align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 20.76
+            ">
+                <g id="圖層_2" data-name="圖層 2">
+                    <g id="圖層_1-2" data-name="圖層 1">
+                        <g id="Group_518" data-name="Group 518">
+                            <path id="Path_424" data-name="Path 424" class="cls-1" d="M19,10.48a10.48,10.48,0,0,0-12.88,0,6.6,6.6,0,0,0-2.8,5c0,3.27,3.54,5.3,9.24,5.3s9.24-2,9.24-5.3A6.6,6.6,0,0,0,19,10.48Z" />
+                            <path id="Path_425" data-name="Path 425" class="cls-1" d="M12.61,6.91A3.47,3.47,0,1,0,8.75,3.47,3.68,3.68,0,0,0,12.61,6.91Z" />
+                            <path id="Path_426" data-name="Path 426" class="cls-1" d="M25,5.85a4.26,4.26,0,0,0-4.61-2.66,3.15,3.15,0,0,0-2.66,3.55,3,3,0,0,0,.2.72A4.13,4.13,0,0,0,19.74,9.6a4.34,4.34,0,0,0,2.11.58,3.68,3.68,0,0,0,.66-.06,3.12,3.12,0,0,0,2.66-3.55A3.06,3.06,0,0,0,25,5.85Z" />
+                            <path id="Path_427" data-name="Path 427" class="cls-1" d="M7.25,7.46A3.12,3.12,0,0,0,5.51,3.39a3.09,3.09,0,0,0-.72-.2A4.12,4.12,0,0,0,2,3.72,3.65,3.65,0,0,0,.37,8.59h0a3.25,3.25,0,0,0,2.27,1.53,3.68,3.68,0,0,0,.66.06A4.14,4.14,0,0,0,5.41,9.6,4.07,4.07,0,0,0,7.25,7.46Z" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+            <span>
+                <h6 class="des-title brown-color t-xxl m-0">Petliday是什麼</h6>
+            </span>
+        </div>
+    </div>
+    <!-- 內容的部分 -->
+    <div class="container">
+        <div class="row d-flex flex-start">
+            <div class="box pro-ppl col-2 text-center b-b">
+                <p>專業人員</p>
+            </div>
+            <div class="box safe-place col-2 text-center b-b">
+                <p>安心場地</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="box info-cont b-b">
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Petliday是什麼的部分末 -->
+<!-- Petliday做了什麼的部分始 -->
+<section>
+
+</section>
+<!-- Petliday做了什麼的部分末 -->
+
+<!-- ------------------ body結束 ------------------ -->
+<?php include __DIR__ . '/../parts/html-footer.php' ?>
+<!-- ---------------js/jq 開始 ------------------ -->
+<?php include __DIR__ . '/../parts/html-script.php' ?>
+<script>
+    $('.c4').on('mouseenter', function() {
+        $(this).find('.card-pic').css('height', '90px');
+        $(this).find('.card-info').css({
+            'opacity': '1',
+            'height': '10px',
+        });
+    });
+    $('.c4').on('mouseleave', function() {
+        $(this).find('.card-pic').css('height', '140px');
+        $(this).find('.card-info').css({
+            'opacity': '0',
+            'height': '0',
+        });
+    });
+
+    // ------JS開始 以上勿刪-------
+
+
+
+    // ------JS結束 勿刪到-------
+</script>
+<?php include __DIR__ . '/../parts/html-foot.php' ?>
