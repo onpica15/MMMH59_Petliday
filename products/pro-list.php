9a6a6086c0d470c6cbbd1d281d1c94eb45f1eec9
millie0 <ricebug19@gmail.com>
2020-11-24 17:00:29 +0800

    🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂

--- products/pro-list.php
+++ products/pro-list.php
@@ -633,6 +633,7 @@
 <?php include __DIR__ . '/../parts/html-script.php' ?>
 <!-- hot carosel -->
 <script>
+    // 熱門輪播 hot carosel
     let index = 1;
     $('.dots li').first().css('background', '#ffc072');
 
@@ -648,14 +649,13 @@
         // let nowIndex = (index > 4) ? 0 : index;
         $('.wrap-img').css('transition', '0.7s').css('left', (-1140 * index));
         $('.dots li').eq(index - 1).css('background', '#ffc072').siblings().css('background', '#ccc');
-
-
         $('.wrap-img').on('transitionend webkitTransitionEnd', function() {
-            if (index == 4) {
+            if (index == 5) {
                 index = 0;
-                console.log('index2:', index);
-                $('.wrap-img').css('transition', 'none').css('left', '0')
                 $('.dots li').eq(index).css('background', '#ffc072').siblings().css('background', '#ccc');
+                index = 1;
+                console.log('index2:', index);
+                $('.wrap-img').css('transition', 'none').css('left', '-1140px')
             }
         });
     });
@@ -674,6 +674,50 @@
             }
         });
     });
+    // rwd時候的熱門輪播 rwd425 hot carosel
+    if (window.matchMedia('(max-width: 425px)').matches) {
+        let index = 1;
+        $('.dots li').first().css('background', '#ffc072');
+
+        $('.dots li').click(function() {
+            $(this).css('background', '#ffc072').siblings().css('background', '#ccc');
+
+            $('.wrap-img').css('left', -340 * ($(this).index() + 1));
+            index = $(this).index();
+        });
+        $('.ar').click(function() {
+            index = index + 1;
+            console.log('index1:', index);
+            // let nowIndex = (index > 4) ? 0 : index;
+            $('.wrap-img').css('transition', '0.7s').css('left', (-340 * index));
+            $('.dots li').eq(index - 1).css('background', '#ffc072').siblings().css('background', '#ccc');
+
+
+            $('.wrap-img').on('transitionend webkitTransitionEnd', function() {
+                if (index == 4) {
+                    index = 0;
+                    console.log('index2:', index);
+                    $('.wrap-img').css('transition', 'none').css('left', '-40')
+                    $('.dots li').eq(index).css('background', '#ffc072').siblings().css('background', '#ccc');
+                }
+            });
+        });
+        $('.al').click(function() {
+            index = index - 1;
+            // let nowIndex = (index < 0) ? 4 : index;
+            $('.wrap-img').css('transition', '0.7s').css('left', -340 * (index + 1));
+            $('.dots li').eq(index).css('background', '#ffc072').siblings().css('background', '#ccc');
+
+            $('.wrap-img').on('transitionend webkitTransitionEnd', function() {
+                if (index == -1) {
+                    index = 4;
+                    console.log('index4:', index);
+                    $('.wrap-img').css('transition', 'none').css('left', '-4560px')
+                    $('.dots li').eq(index).css('background', '#ffc072').siblings().css('background', '#ccc');
+                }
+            });
+        });
+    }
 </script>
 <!-- 愛心 收藏喜好清單 -->
 <script>
