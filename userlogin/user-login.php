62791b809f70af6bed62a753326ba098fbc09860
YuChieh <la0603c@gmail.com>
2020-11-25 19:54:22 +0800

    login-rwd

--- userlogin/user-login.php
+++ userlogin/user-login.php
@@ -1,29 +1,20 @@
 <?php include __DIR__ . '/../parts/config.php' ?>
+<?php
+if(isset($_POST['account']) and isset($_POST['password'])){
+  if($_POST['account']==='boy123@petliday.com' and $_POST['password']==='test123');
+  else{
+    $msg = '用戶帳號或密碼不正確';
+  }
+}
+?>
+
+
 <?php include __DIR__ . '/../parts/html-head.php' ?>
 <!-- --- css 連結放下面 ----- -->
 <link rel="stylesheet" href="user-login.css">
 
 <?php include __DIR__ . '/../parts/html-navbar.php' ?>
 <!-- ------------------ body開始 以上勿刪 ------------------ -->
-<div class="container mt-5">
-
-  <h1 class="orange-color t-xxxl bg-color ">Hello!開始開心寫網站 orange-color t-xxxl bg-color</h1>
-  <br>
-  <h6 class="brown-color t-xxl ">毛日 brown-color t-xxl</h6>
-  <br>
-  <h6 class="green-color t-xl">毛日 green-color t-xl</h6>
-  <br>
-  <h6 class="danger-color t-l">毛日 danger-color t-l</h6>
-  <br>
-  <h6 class="text-color t-m">毛日 text-color t-m</h6>
-  <br>
-  <h6 class="text-color t-s">毛日 text-color t-s</h6>
-  <br>
-  <h6 class="text-gray t-xs">毛日 text-gray t-xs</h6>
-
-  <button class="btn">測試 class='btn'</button>
-
-</div>
 
 <!-- Model -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -63,6 +54,12 @@
             <input type="password" class="form-control" id="password" name="password" placeholder="密碼">
             <img class="icon-psd" src="./imgs/password.svg">
           </div>
+
+          <?php if(isset($msg)): ?>
+            <div>
+              <?= $msg ?>
+            </div>
+          <?php endif; ?>
 
           <div class="login-group-2">
             <button class="login-btn btn">登入</button>
