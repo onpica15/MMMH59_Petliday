c7e6f4a397cf102c8765dd76efdf02c9b9de9d6e
millie0 <ricebug19@gmail.com>
2020-11-28 01:47:02 +0800

    omgomgomg

--- products/pdlist-db-api.php
+++ products/pdlist-db-api.php
@@ -3,7 +3,7 @@
 $output = [];
 
 
-$getWhere = isset($_GET['`cate`']) ? ($_GET['`cate`']) : '';
+$getWhere = isset($_GET['where']) ? ($_GET['where']) : '';
 // $area = isset($_GET[`area`]) ? intval($_GET['`area`']) : 0;
 
 // 印出抓到的資料ex:{"where": "2 AND `cate`=4 AND `area`='s'"}
@@ -18,73 +18,76 @@ $getWhere = isset($_GET['`cate`']) ? ($_GET['`cate`']) : '';
 
 //tag+++++++++++++++++++++++++++++++++++++++ 
 
-$where = " WHERE `cate`=$getWhere";
-echo $where;
+$where = " WHERE ($getWhere)";
+// echo $where;
 
-// $output['dTime'] = $time;
-// $output['dArea'] = $area;
-// $where .= " AND `cate`=$time ";
+// // $output['dTime'] = $time;
+// // $output['dArea'] = $area;
+// // $where .= " AND `cate`=$time ";
 
 
 $ta_sql = " SELECT * FROM products $where ";
 $ta_row = $pdo->query($ta_sql)->fetchAll();
 
-?>
-<?php foreach ($ta_row as $ta) : ?>
-    <button type="button" class="cate-btn btn btn-outline-primary" data-sid="<?= $ta['sid'] ?>">
-        <?= $ta['star'] ?>
-    </button>
-<?php endforeach ?>
+echo json_encode($ta_row, JSON_UNESCAPED_UNICODE);
+exit;
+// 
 
-<?php exit;
+// <?php foreach ($ta_row as $ta) : >
+    // <button type="button" class="cate-btn btn btn-outline-primary" data-sid="<?= $ta['sid'] >">
+        // <?= $ta['star'] >
+        // </button>
+    // <?php endforeach >
 
-if (!isset($_SESSION['prolist'])) {
-    $_SESSION['prolist'] = [];
-}
+// <?php exit;
 
-
-
-
-$perPage = 9; // 每頁幾筆
-$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-$cate = isset($_GET['cate']) ? intval($_GET['cate']) : 3;
-
-// 取得分類（分類目錄按鈕標題）
-$c_sql = "SELECT * FROM `pd_time` ORDER BY `pd_time`.`sid` ASC";
-$c_rows = $pdo->query($c_sql)->fetchAll();
-
-$where = " WHERE 1 ";
-// if (!empty($cate)) {
-$params['cate'] = $cate;
-$where .= " AND `cate`=$cate ";
-// }
+    // if (!isset($_SESSION['prolist'])) {
+    //     $_SESSION['prolist'] = [];
+    // }
 
 
-// $t_sql = "SELECT COUNT(1) FROM products";
-$t_stmt = $pdo->query($t_sql);
 
-$totalRows = $t_stmt->fetch(PDO::FETCH_NUM)[0]; // 總筆數
-$totalPages = ceil($totalRows / $perPage); // 總頁數
 
-if ($totalRows != 0) {
-    if ($page < 1) {
-        header('Location: pdlist-db.php');
-        exit;
-    }
-    if ($page > $totalPages) {
-        header('Location: pdlist-db.php?page=' . $totalPages);
-        exit;
-    }
-    $sql = sprintf(
-        "SELECT * FROM products %s ORDER BY sid ASC LIMIT %s, %s",
-        $where,
-        ($page - 1) * $perPage,
-        $perPage
-    );
+    // $perPage = 9; // 每頁幾筆
+    // $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    // $cate = isset($_GET['cate']) ? intval($_GET['cate']) : 3;
 
-    $stmt = $pdo->query($sql);
-    $rows = $stmt->fetchAll();
-} else {
-    $rows = [];
-}
-?>
+    // // 取得分類（分類目錄按鈕標題）
+    // $c_sql = "SELECT * FROM `pd_time` ORDER BY `pd_time`.`sid` ASC";
+    // $c_rows = $pdo->query($c_sql)->fetchAll();
+
+    // $where = " WHERE 1 ";
+    // // if (!empty($cate)) {
+    // $params['cate'] = $cate;
+    // $where .= " AND `cate`=$cate ";
+    // // }
+
+
+    // // $t_sql = "SELECT COUNT(1) FROM products";
+    // $t_stmt = $pdo->query($t_sql);
+
+    // $totalRows = $t_stmt->fetch(PDO::FETCH_NUM)[0]; // 總筆數
+    // $totalPages = ceil($totalRows / $perPage); // 總頁數
+
+    // if ($totalRows != 0) {
+    //     if ($page < 1) {
+    //         header('Location: pdlist-db.php');
+    //         exit;
+    //     }
+    //     if ($page > $totalPages) {
+    //         header('Location: pdlist-db.php?page=' . $totalPages);
+    //         exit;
+    //     }
+    //     $sql = sprintf(
+    //         "SELECT * FROM products %s ORDER BY sid ASC LIMIT %s, %s",
+    //         $where,
+    //         ($page - 1) * $perPage,
+    //         $perPage
+    //     );
+
+    //     $stmt = $pdo->query($sql);
+    //     $rows = $stmt->fetchAll();
+    // } else {
+    //     $rows = [];
+    // }
+    // 
--- products/pdlist-db.php
+++ products/pdlist-db.php
@@ -1,28 +1,4 @@
 <?php include __DIR__ . '/../parts/config.php' ?>
-<?php
-if ($totalRows != 0) {
-    // if ($page < 1) {
-    //     header('Location: pdlist-db.php');
-    //     exit;
-    // }
-    // if ($page > $totalPages) {
-    //     header('Location: pdlist-db.php?page=' . $totalPages);
-    //     exit;
-    // }
-    $sql = sprintf(
-        "SELECT * FROM products %s ORDER BY sid ASC LIMIT %s, %s",
-        $where,
-        ($page - 1) * $perPage,
-        $perPage
-    );
-
-    $stmt = $pdo->query($sql);
-    $rows = $stmt->fetchAll();
-} else {
-    $rows = [];
-}
-?>
-
 <?php include __DIR__ . '/../parts/html-head.php' ?>
 <link rel="stylesheet" href="pro-list.css">
 <?php include __DIR__ . '/../parts/html-navbar.php' ?>
@@ -41,10 +17,10 @@ if ($totalRows != 0) {
     </div> -->
     <div class="time align-items-center">
         <p class="pr-3">玩多久？</p>
-        <a class="tag-not-on" data-time="3">三日遊</a>
-        <a class="tag-not-on" data-time="2">二日遊</a>
-        <a class="tag-not-on" data-time="1">一日遊</a>
-        <a class="tag-not-on" data-time="4">活動</a>
+        <a class="tag-not-on tag-on" data-time="3">三日遊</a>
+        <a class="tag-not-on tag-on" data-time="2">二日遊</a>
+        <a class="tag-not-on tag-on" data-time="1">一日遊</a>
+        <a class="tag-not-on tag-on" data-time="4">活動</a>
     </div>
     <div class="area align-items-center">
         <p class="pr-3">去哪玩？</p>
@@ -66,45 +42,7 @@ if ($totalRows != 0) {
 </div>
 <!-- card -->
 <div class="container">
-    <div class="row align-items-center">
-        <?php foreach ($rows as $r) : ?>
-            <div class="product-item col-md-4 col-12 p-4" data-sid="<?= $r['sid'] ?>">
-                <div class="card c3">
-                    <div class="heart-circle">
-                        <div class="heart"><img src="/Petliday/icon/heart-red.png" alt=""></div>
-                    </div>
-                    <div class="card-pic w-100">
-                        <img src="/Petliday/products/img/prolist<?= $r['sid'] ?>.jpg" alt="...">
-                        <!-- <img src="/Petliday/products/img/prolist1.jpg" alt=""> -->
-                    </div>
-                    <div class="card-text pt-3 pb-1 px-4">
-                        <p><?= $r['product_name'] ?></p>
-                        <div class="info d-flex justify-content-between">
-                            <div class="info-left d-flex align-items-center">
-                                <div class="star mb-2 mr-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66.3 63.4" width="14px">
-                                        <defs>
-                                        </defs>
-                                        <g id="圖層_2" data-name="圖層 2">
-                                            <g id="圖層_1-2" data-name="圖層 1">
-                                                <path class="cls-3" d="M51.47,63.4A3.55,3.55,0,0,1,49.84,63L33.15,54.22,16.45,63a3.49,3.49,0,0,1-5.07-3.69l3.18-18.59L1.06,27.55a3.5,3.5,0,0,1,1.94-6l18.66-2.71L30,2a3.5,3.5,0,0,1,6.28,0l8.35,16.92L63.3,21.58a3.5,3.5,0,0,1,1.94,6L51.74,40.71,54.92,59.3a3.5,3.5,0,0,1-3.45,4.1Z" />
-                                            </g>
-                                        </g>
-                                    </svg>
-
-                                </div>
-                                <div class="rate text-gray t-m"><?= $r['star'] ?></div>
-                                <div class="rate-all t-xs ml-2"><u class="text-gray"><?= $r['rate'] ?>則評論 </u></div>
-                            </div>
-                            <div class="info-right pr-2 t-l orange-color">$<?= $r['price_all'] ?></div>
-                        </div>
-                    </div>
-                    <p class="card-info px-4 t-xs">
-                        <?= $r['intro'] ?>
-                    </p>
-                </div>
-            </div>
-        <?php endforeach; ?>
+    <div class="row product-row align-items-center">
     </div>
 </div>
 <!-- 11/13 研究分頁按鈕連結-->
@@ -151,7 +89,45 @@ if ($totalRows != 0) {
 <?php include __DIR__ . '/../parts/html-script.php' ?>
 <script>
     // ------JS開始 以上勿刪-------
+    const productTpl = function(a) {
+        return `
+            <div class="product-item col-md-4 col-12 p-4" data-sid="${a.sid}">
+            <div class="card c3">
+                <div class="heart-circle">
+                    <div class="heart"><img src="/Petliday/icon/heart-red.png" alt=""></div>
+                </div>
+                <div class="card-pic w-100">
+                    <img src="/Petliday/products/img/prolist${a.sid}.jpg" alt="...">
+                </div>
+                <div class="card-text pt-3 pb-1 px-4">
+                    <p>${a.product_name}</p>
+                    <div class="info d-flex justify-content-between">
+                        <div class="info-left d-flex align-items-center">
+                            <div class="star mb-2 mr-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66.3 63.4" width="14px">
+                                    <defs>
+                                    </defs>
+                                    <g id="圖層_2" data-name="圖層 2">
+                                        <g id="圖層_1-2" data-name="圖層 1">
+                                            <path class="cls-3" d="M51.47,63.4A3.55,3.55,0,0,1,49.84,63L33.15,54.22,16.45,63a3.49,3.49,0,0,1-5.07-3.69l3.18-18.59L1.06,27.55a3.5,3.5,0,0,1,1.94-6l18.66-2.71L30,2a3.5,3.5,0,0,1,6.28,0l8.35,16.92L63.3,21.58a3.5,3.5,0,0,1,1.94,6L51.74,40.71,54.92,59.3a3.5,3.5,0,0,1-3.45,4.1Z" />
+                                        </g>
+                                    </g>
+                                </svg>
 
+                            </div>
+                            <div class="rate text-gray t-m">${a.star}</div>
+                            <div class="rate-all t-xs ml-2"><u class="text-gray">${a.rate}則評論 </u></div>
+                        </div>
+                        <div class="info-right pr-2 t-l orange-color">$${a.price_all}</div>
+                    </div>
+                </div>
+                <p class="card-info px-4 t-xs">
+                    ${a.intro}
+                </p>
+            </div>
+        </div>
+        `;
+    }
     // tag selection
     $('.time a, .area a').on('click', function() {
         $(this).toggleClass('tag-on');
@@ -190,31 +166,52 @@ if ($totalRows != 0) {
         console.log('whereArray', whereArray);
         if (sendTimeStr.length !== 0 && sendAreaStr.length !== 0) {
             console.log('hi')
-            whereStr = whereArray.join(" AND ");
+            whereStr = whereArray.join(") AND (");
         } else {
             console.log('nono')
             whereStr = whereArray.join("");
         }
         console.log('whereStr', whereStr);
+        location.href = "#" + whereStr;
 
+        $.get('pdlist-db-api.php', {
+                where: whereStr,
+            },
+            function(data) {
+                console.log('daaaaaaa', data);
 
-        // const sendDataStr = sendData.join(' OR ');
-        // console.log(sendDataStr);
-        $.get('pdlist-db-api.php'), {
-            // where: whereStr,
-            time: sendTime,
-            area: sendArea
-        }
+                let str = '';
+                if (data && data.length) {
+                    data.forEach(function(el) {
+                        str += productTpl(el);
+                    });
+                }
+                $('.product-row').html(str);
+            }, 'json');
 
-        // location.href = "#" + whereStr;
-        // $.get('pdlist-db-api.php?' + whereStr, function(data) {
-        //     console.log('last', whereStr);
-        //     // getProductData(u);
-        // }, 'json');
     })
+    $.get('pdlist-db-api.php', {
+            where: whereStr,
+        },
+        function(data) {
+            console.log('daaaaaaa', data);
+
+            let str = '';
+            if (data && data.length) {
+                data.forEach(function(el) {
+                    str += productTpl(el);
+                });
+            }
+            $('.product-row').html(str);
+        }, 'json');
+
+
+
+
+
 
     // from 19.product-list-ajax.php+++++++++++++++++++++++
-    const tags = $('.time p, .area p');
+
 
 
     // function whenHashChanged() {
