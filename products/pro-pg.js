e9f479ed7ebe43e05029d6d551df8d8f42649427
millie0 <ricebug19@gmail.com>
2020-11-30 21:16:22 +0800

    pro-pg卡肉搜

--- products/pro-pg.js
+++ products/pro-pg.js
@@ -1,7 +1,21 @@
+// variable
+let isScroll = true;
+let gotoPage = 0;
+let index = 0;
 
 // D clip path**********************************
 
 $('#d-clip-web').attr('clipPathUnits', 'objectBoundingBox').attr('transform', 'scale(0.0019 0.00178)');
+
+// D opacity carosel
+$('.caro img').on('click',function(){
+    console.log('hi',$(this).index())
+})
+setInterval(function(){
+    index = index + 1;
+    (index > 3) ? index=0 : index=index;
+    $('.caro img').eq(index).css('opacity','1').siblings().css('opacity','0');
+},2000);
 
 //  上面大愛心點按加入取消清單**********************************
 
@@ -48,12 +62,42 @@ $('.share-row').on('click',function(){
 })
 
 
+//選擇區塊資料丟進session['cart'] start**********************************
+
+$('.btn-twin').on('click', function() {
+    const sid = $('.heart-top').attr('data-sid');
+    const manQ = $('#man-quantity').val();
+    const petQ = $('#pet-quantity').val();
+    const date = $('.calendar .active').children('p').html();
+
+    console.log('sid', sid);
+    console.log('manQ', manQ);
+    console.log('petQ', petQ);
+    console.log('date', date);
+
+    $.get('pro-pg-api.php', {
+        sid: sid,
+        manQ: manQ,
+        petQ: petQ,
+        date: date,
+        item: 1,
+        action: 'add'
+    },
+    function(data) {
+        console.log(data);
+        countCart(data);
+    }, 'json');
+
+})
+
+//選擇區塊資料丟進session['cart'] end**********************************
+
 //calendar點日期換背景色**********************************
 
 // const td = document.querySelectorAll('td')
 $('.calendar td').on('click',function(){
-    $('#data').find('td').css('background-color','#fff');
-    $(this).css('background-color','#ffc072');
+    $('#data').find('td').css('background-color','#fff').removeClass('active');
+    $(this).css('background-color','#ffc072').addClass('active');
     // console.log('this.val()',td.innerHTML);
     // console.log('this.val()',dataTemp[i]);
 })
@@ -154,41 +198,57 @@ const subPetBtn = document.getElementById('sub-pet')
 
 
 // cate menu 行程介紹fixed目錄bar 滑到定位固定**********************************
-
-$(window).scroll(function () {
-    let nowTop = $(this).scrollTop();
-    if($(this).scrollTop() >= 1835 ){
-        $('.cate-fix').css('position','fixed').css('top','63px').css('left','405px')
-    }
-    else{
-        $('.cate-fix').css('position','relative').css('top','0').css('left','0')
-    }
-})
 if (window.matchMedia('(max-width: 425px)').matches) {
+    // 手機版
     $(window).scroll(function () {
         let nowTop = $(this).scrollTop();
-        if($(this).scrollTop() >= 1795 ){
+        if($(this).scrollTop() >= 1932 ){
             $('.cate-fix').css('position','fixed').css('top','63px').css('left','15px')
+            // $('.trip-section').css('padding-top','80px');
+            $('.main-c').css('padding-top','80px');
         }
         else{
-            $('.cate-fix').css('position','relative').css('top','0')
+            $('.cate-fix').css('position','relative').css('top','0').css('left','0px')
+            // $('.trip-section').css('padding-top','0px');
+            $('.main-c').css('padding-top','0px');
+        }
+    });
+    // 網頁版
+} else {
+    $(window).scroll(function () {
+        let nowTop = $(this).scrollTop();
+        if($(this).scrollTop() >= 1833 ){
+            $('.cate-fix').css('position','fixed').css('top','63px').css('left','405px')
+            $('.bg-dog').css('padding-top','80px');
+            // $('.day123-fix').css('top','200px');
+        }
+        else{
+            $('.cate-fix').css('position','relative').css('top','0').css('left','0')
+            $('.bg-dog').css('padding-top','0px');
+            
         }
     })
 }
 // 123day fixed**********************************
-
+if (window.matchMedia('(max-width: 425px)').matches) {
 $(window).scroll(function () {
-    let nowTop = $(this).scrollTop();
-    if(nowTop >= 1835 && nowTop <= 6530){
-        $('.day123-fix').css('position','fixed').css('top','200px').css('right','-20px')
-    }
-    else if(nowTop > 6530){
-        $('.day123-fix').css('position','relative').css('top','4800px').css('right','-20px')
-    }
-    else{
-        $('.day123-fix').css('position','relative').css('top','200px').css('right','-20px')
-    }
+        $('.day123-fix').css('position','relative').css('top','0px').css('right','0px')
 })
+// 網頁版
+}else{
+    $(window).scroll(function () {
+        let nowTop = $(this).scrollTop();
+        if(nowTop >= 1833 && nowTop <= 6530){
+            $('.day123-fix').css('position','fixed').css('top','140px').css('right','-20px')
+        }
+        else if(nowTop > 6530){
+            $('.day123-fix').css('position','relative').css('top','4800px').css('right','-20px')
+        }
+        else{
+            $('.day123-fix').css('position','relative').css('top','0px').css('right','-20px')
+        }
+    })
+}
 // 123day 滑到區域換色**********************************
 
 $(window).scroll(function () {
@@ -213,33 +273,89 @@ $('.btn-cate').on('click',function(){
 $(window).scroll(function () {
     let nowTop = $(this).scrollTop();
     console.log('scrolltop', nowTop)
-    if(nowTop>1835 && nowTop<6830){
-        $('.bc1').css('background-color','#ffc072').siblings().css('background-color','#fff');
+
+    switch (gotoPage){
+        case 0:
+            if(nowTop === 1900) isScroll = true;
+            break;
+        case 1:
+            if(nowTop === 7045) isScroll = true;
+            break;
+        case 2:
+            if(nowTop === 7380) isScroll = true;
+            break;
+        case 3:
+            if(nowTop === 7790) isScroll = true;
+            break;
+        case 4:
+            if(nowTop === 8460) isScroll = true;
+            break;
+        case 5:
+            if(nowTop === 9418) isScroll = true;
+            break;
+
+        default:
+            break;
     }
-    else if(nowTop>6830 && nowTop<7280){
-        $('.bc2').css('background-color','#ffc072').siblings().css('background-color','#fff');
+
+
+    if(isScroll){
+        if(nowTop>1835 && nowTop<6830){
+            $('.bc1').css('background-color','#ffc072').siblings().css('background-color','#fff');
+        }
+        else if(nowTop>6830 && nowTop<7280){
+            $('.bc2').css('background-color','#ffc072').siblings().css('background-color','#fff');
+        }
+        else if(nowTop>7280 && nowTop<7760){
+            $('.bc3').css('background-color','#ffc072').siblings().css('background-color','#fff');
+        }
+        else if(nowTop>7760 && nowTop<8330){
+            $('.bc4').css('background-color','#ffc072').siblings().css('background-color','#fff');
+        }
+        else if(nowTop>8330 && nowTop<9200){
+            $('.bc5').css('background-color','#ffc072').siblings().css('background-color','#fff');
+        }
+        else if(nowTop>9200){
+            $('.bc6').css('background-color','#ffc072').siblings().css('background-color','#fff');
+        }
     }
-    else if(nowTop>7280 && nowTop<7760){
-        $('.bc3').css('background-color','#ffc072').siblings().css('background-color','#fff');
-    }
-    else if(nowTop>7760 && nowTop<8330){
-        $('.bc4').css('background-color','#ffc072').siblings().css('background-color','#fff');
-    }
-    else if(nowTop>8330 && nowTop<9200){
-        $('.bc5').css('background-color','#ffc072').siblings().css('background-color','#fff');
-    }
-    else if(nowTop>9200){
-        $('.bc6').css('background-color','#ffc072').siblings().css('background-color','#fff');
-    }
+    
 })
 
 // <!-- term點了展開 -->**********************************
 
-
+if (window.matchMedia('(max-width: 425px)').matches) {
+    // 手機版
+    $('.t1').on('click', function() {
+        console.log('height', $(this).css('height'));
+        if ($(this).css('height') === '300px') {
+            $(this).css('height', '960px').find('.hidden').css('display', 'inline-block');
+        } else {
+            $(this).css('height', '300px').find('.hidden').css('display', 'none');
+        }
+    });
+    $('.t2').on('click', function() {
+        console.log('height', $(this).css('height'));
+        if ($(this).css('height') === '300px') {
+            $(this).css('height', '620px').find('.hidden').css('display', 'inline-block');
+        } else {
+            $(this).css('height', '300px').find('.hidden').css('display', 'none');
+        }
+    });
+    $('.t3').on('click', function() {
+        console.log('height', $(this).css('height'));
+        if ($(this).css('height') === '220px') {
+            $(this).css('height', '560px').find('.hidden').css('display', 'inline-block');
+        } else {
+            $(this).css('height', '220px').find('.hidden').css('display', 'none');
+        }
+    });
+    // 電腦版
+}else{
     $('.t1').on('click', function() {
         console.log('height', $(this).css('height'));
         if ($(this).css('height') === '350px') {
-            $(this).css('height', '790px').find('.hidden').css('display', 'inline-block');
+            $(this).css('height', '790px').find('.hidden').css('display', 'inline-block'); 
         } else {
             $(this).css('height', '350px').find('.hidden').css('display', 'none');
         }
@@ -252,6 +368,7 @@ $(window).scroll(function () {
             $(this).css('height', '350px').find('.hidden').css('display', 'none');
         }
     });
+}
         $('.c3').on('mouseenter', function() {
             $(this).find('.card-pic').css('height', '135px');
             $(this).find('.card-info').css({
@@ -280,3 +397,55 @@ $(window).scroll(function () {
             $(this).find('img').attr('src', '/Petliday/icon/heart-red.png');
         }
     });
+
+
+
+    $('.btn-cate').on('click',function(){
+        // console.log('hi',$(this).index())
+        
+        isScroll = false;
+
+        let index = $(this).index();
+        switch (index){
+            case 0:
+                gotoPage = 0;
+                break;
+            case 1:
+                gotoPage = 1;
+                break;
+            case 2:
+                gotoPage = 2;
+                break;
+            case 3:
+                gotoPage = 3;
+                break;
+            case 4:
+                gotoPage = 4;
+                break;
+            case 5:
+                gotoPage = 5;
+                break;
+
+            default:
+                break;
+        }
+
+    })
+
+    $('.btn-cate').on('mouseenter',function(){
+        if($(this).css('background-color') === 'rgb(255, 192, 114)'){
+        }
+        else{
+            $(this).css('background-color','#ffe0b9')
+        }
+    })
+
+    $('.btn-cate').on('mouseleave',function(){
+        if($(this).css('background-color') === 'rgb(255, 192, 114)'){
+            console.log('hi #ffc072')
+        }
+        else{
+            $(this).css('background-color','#fff')
+        }
+
+    })
