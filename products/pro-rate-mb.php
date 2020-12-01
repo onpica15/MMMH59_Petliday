2813be835000b642a60bee29ea9e5cb9d14bfea6
millie0 <ricebug19@gmail.com>
2020-12-01 11:18:47 +0800

    pro-rate-mb

---
+++ products/pro-rate-mb.php
@@ -0,0 +1,261 @@
+<?php include __DIR__ . '/../parts/config.php' ?>
+<?php include __DIR__ . '/../parts/html-head.php' ?>
+<link rel="stylesheet" href="pro-rate-mb.css">
+<?php include __DIR__ . '/../parts/html-navbar.php' ?>
+<div class="container">
+    <div class="top-wrap info">
+        <!-- 麵包屑 -->
+        <div class="row bread-row mb-2 t-xs">
+            <a href="">首頁</a> /
+            <a href="">行程列表</a> /
+            <a href="">台北出發</a> /
+            <a href="">花東三日遊</a> /
+            <a href="">行程評價</a>
+        </div>
+        <!-- 標題row -->
+        <div class="row title-row">
+            <div class="title-left">
+                <p>上山下海玩得夠！｜熱氣球嘉年華＆</p>
+                <p>SUP體驗＆金針花海 花東三日遊</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="product w-100">
+    <div class="bg-dog">
+        <div class="container main-c pt-4">
+            <div class="topic topic-intro">
+                <p>行程評價</p>
+                <p class="topic-line"></p>
+            </div>
+            <div class="rate-card">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar1.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs">愛莉師</p>
+                    <p class="rate-title t-m mb-2">台東熱氣球嘉年華</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">很幸運的看到起飛的景象，超級美的</p>
+                    <p class="t-xs mb-1">狂拍照留念，第一站就佔了很多手機記憶體耶</p>
+                    <p class="t-xs mb-1">明年想要再帶我們家的小可愛來看！</p>
+                </div>
+            </div>
+            <div class="rate-card c2">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar2.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">John</p>
+                    <p class="rate-title t-m mb-2">露營區很棒</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">可以跟三五好友帶著寵物來露營，真的是很棒的體驗</p>
+                    <p class="t-xs mb-1">營區還有特別準備寵物的食材，非常貼心！</p>
+                </div>
+            </div>
+            <div class="rate-card c3">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar3.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-2">Lily Alien</p>
+                    <p class="rate-title t-m mb-2">六十石山金針花海</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">有非常充裕的時間可以在這裡狂殺底片</p>
+                    <p class="t-xs mb-1">這裡實在太美了！</p>
+                    <p class="t-xs mb-1">我跟我們家的寵物也陷入網美的世界了 (害羞)</p>
+                </div>
+            </div>
+            <div class="rate-card c4">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar4.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">蝦哩</p>
+                    <p class="rate-title t-m mb-2">三天兩夜玩得超開心</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">旅行社真的太貼心了，行程充裕不緊湊。</p>
+                    <p class="t-xs mb-1">跟自己的寵物體驗上山下海的行程很難得！</p>
+                    <p class="t-xs mb-1">在這裡認識好多狗友們，開心 :)</p>
+
+                </div>
+            </div>
+            <div class="rate-card c5">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar5.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">木瓜人</p>
+                    <p class="rate-title t-m mb-2">都歷海灘SUP</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">之前在網路上看到別人分享和狗狗一起衝浪的體驗</p>
+                    <p class="t-xs mb-1">想不到今天我實現了！！！</p>
+                    <p class="t-xs mb-1">而且有專業教練的帶領，可以非常安心的盡興玩樂！</p>
+                </div>
+            </div>
+            <div class="rate-card c6">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar6.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">Tom Cat</p>
+                    <p class="rate-title t-m mb-2">住宿與食物</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">旅行社選的住宿景點，對寵物都是非常的友善</p>
+                    <p class="t-xs mb-1">食物也會精心準備，完全不馬虎耶！吃得超開心</p>
+                    <p class="t-xs mb-1">CP值超級高的行程，推推</p>
+                </div>
+            </div>
+            <div class="rate-card c7">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar7.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-2">柏拉圖</p>
+                    <p class="rate-title t-m mb-2">值得體驗的行程</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">浮潛真的太棒了，海底的珊瑚礁非常漂亮！</p>
+                    <p class="t-xs mb-1">天氣超熱，遊覽車絕對非常適合</p>
+                    <p class="t-xs mb-1">搭中巴環七美島，司機時間抓的很充裕，也都會解說</p>
+                </div>
+            </div>
+            <div class="rate-card c8">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar8.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">Silver Gray</p>
+                    <p class="rate-title t-m mb-2">cp值頗高的單日旅遊</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">我們這團才6人，所以旅遊品質超棒的啦！</p>
+                    <p class="t-xs mb-1">小烏來風景區可以從玻璃天空步道欣賞壯闊的瀑布景觀</p>
+                    <p class="t-xs mb-1">導遊大哥一路上很細心為我們解說，體貼友善</p>
+
+                </div>
+            </div>
+            <div class="rate-card c9">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar9.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">比利獸</p>
+                    <p class="rate-title t-m mb-2">免裝備豪華露營</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">第一次體會豪華露營，真的很不錯，營區環境很舒服</p>
+                    <p class="t-xs mb-1">暖東峽谷步道整理的很好，很適合全家大小一起去散步</p>
+                    <p class="t-xs mb-1">餐食很用心，吃的很豐盛，有機會會再去住一晚。</p>
+                </div>
+            </div>
+            <div class="rate-card c10">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar10.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">Pitty Girl</p>
+                    <p class="rate-title t-m mb-2">風景優美</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">自備食材。環境乾淨舒適。 很棒的懶人露營</p>
+                    <p class="t-xs mb-1">提供的備品也都很齊全、設想周到 會推薦朋友來
+                    </p>
+                </div>
+            </div>
+            <div class="rate-card c11">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar11.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-2">阿蓉</p>
+                    <p class="rate-title t-m mb-2">汪汪地瓜園控窯</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">非常愉快的控窯體驗，食材非常的充足</p>
+                    <p class="t-xs mb-1">導覽哥哥講解的很生動，朋友都非常踴躍發言</p>
+                    <p class="t-xs mb-1">謝謝店家的用心讓我們渡過了愉快的週末！</p>
+                </div>
+            </div>
+            <div class="rate-card c12">
+                <div class="rate-l m-3"><img src="/Petliday/products/img/avatar12.jpg" alt="">
+                </div>
+                <div class="rate-r m-2">
+                    <p class="rate-name t-xs mt-3">迷唇妹</p>
+                    <p class="rate-title t-m mb-2">麻雀雖小，五臟俱全</p>
+                    <div class="star d-flex mb-3">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                        <img src="/petliday/icon/star-green-fill.png" alt="">
+                    </div>
+                    <p class="t-xs mb-1">食材豐盛好吃，環境乾淨舒適。</p>
+                    <p class="t-xs mb-1">店家細心解說流程，還預備了許多孩子能參與的活動</p>
+                    <p class="t-xs mb-1">環境非常好，腹地廣大，很適合帶毛孩一起去玩樂</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ------------------ body結束 ------------------ -->
+<?php include __DIR__ . '/../parts/html-footer.php' ?>
+<!-- ---------------js/jq 開始 ------------------ -->
+<?php include __DIR__ . '/../parts/html-script.php' ?>
+
+<!-- // ------JS開始 以上誤刪------- -->
+<script>
+</script>
+<!-- // ------JS結束 誤刪到------- -->
+
+<?php include __DIR__ . '/../parts/html-foot.php' ?>
