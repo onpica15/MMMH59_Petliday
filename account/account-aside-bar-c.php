e2c7ca7e9d9117171a18029fbc7f0c57d3b20de0
iiujung <foradadad@gmail.com>
2020-12-11 11:11:46 +0800

    addicon2

--- account/account-aside-bar-c.php
+++ account/account-aside-bar-c.php
@@ -6,12 +6,149 @@
                 <div class="menu-bar list-group text-center t-l" id="list-tab" role="tablist">
 
                     <!-- <div class="menu-bar"> -->
-                    <a class="col-4 col-lg-12 list-group-item list-group-item-action" href=" account-profile.php">會員資料</a>
-                    <a class="col-4 col-lg-12 list-group-item list-group-item-action" href=" account-order.php">我的訂單</a>
-                    <a class="col-4 col-lg-12 list-group-item list-group-item-action" href=" account-wishlist.php">願望清單</a>
-                    <a class="col-4 col-lg-12 list-group-item list-group-item-action" href=" account-mailbox.php">會員信箱</a>
-                    <a class="col-4 col-lg-12 list-group-item list-group-item-action" href=" account-coupon.php">優惠券</a>
-                    <a class="col-4 col-lg-12 list-group-item list-group-item-action" href=" account-qa.php">Q&A</a>
+                    <a class="col-4 col-lg-12 list-group-item list-group-item-action d-flex align-items-center justify-content-center" href=" account-profile.php">
+                        <div class="account-icon mr-2 list-group-icon ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66.67 64.45" id="account-icon-or active">
+                                <defs>
+                                    <style>
+                                        .cls-1 {
+                                            fill: #3c2e26;
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="圖層_2" data-name="圖層 2">
+                                    <g id="圖層_1-2" data-name="圖層 1">
+                                        <g id="圖層_2-2" data-name="圖層 2">
+                                            <g id="圖層_1-2-2" data-name="圖層 1-2">
+                                                <path class="cls-1" d="M43.79,27.43a16,16,0,1,0-22.63-.23l.63.61C9.1,32.79,0,45.76,0,61a3.5,3.5,0,0,0,3.5,3.5H63.17a3.5,3.5,0,0,0,3.5-3.5C66.67,45.35,57.07,32.12,43.79,27.43ZM32.6,7.12a9,9,0,1,1-9,9A9,9,0,0,1,32.6,7.12ZM7.2,57.45c1.61-14,12.72-24.81,26.14-24.81S57.86,43.49,59.47,57.45Z" />
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="t-x list-group-text active">會員資料</div>
+                    </a>
+                    <a class="col-4 col-lg-12 list-group-item list-group-item-action d-flex align-items-center justify-content-center" href=" account-order.php">
+                        <div class="account-icon mr-2 list-group-icon ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60.25 62.85">
+                                <defs>
+                                    <style>
+                                        .cls-1 {
+                                            fill: #3c2e26;
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="圖層_2" data-name="圖層 2">
+                                    <g id="圖層_1-2" data-name="圖層 1">
+                                        <path class="cls-1" d="M57.25,62.85H3a3,3,0,0,1-3-3V3A3,3,0,0,1,3,0H57.25a3,3,0,0,1,3,3V59.85A3,3,0,0,1,57.25,62.85ZM6,56.85H54.25V6H6Z" />
+                                        <path class="cls-1" d="M45.68,22.32H22.6a3,3,0,0,1,0-6H45.68a3,3,0,0,1,0,6Z" />
+                                        <path class="cls-1" d="M15.12,22.32A3,3,0,0,1,13,21.44a2.74,2.74,0,0,1-.38-.45,3.67,3.67,0,0,1-.27-.52,2.78,2.78,0,0,1-.17-.56,3,3,0,0,1-.06-.59,3.16,3.16,0,0,1,.23-1.15,3.1,3.1,0,0,1,.27-.51A2.82,2.82,0,0,1,13,17.2a3.16,3.16,0,0,1,.45-.37,3.74,3.74,0,0,1,.52-.28,3.62,3.62,0,0,1,.56-.17,2.93,2.93,0,0,1,1.18,0,3.62,3.62,0,0,1,.56.17,3.15,3.15,0,0,1,.51.28,2.74,2.74,0,0,1,.83.83,2.53,2.53,0,0,1,.28.51,3.16,3.16,0,0,1,.23,1.15,3,3,0,0,1-.06.59,2.78,2.78,0,0,1-.17.56,2.92,2.92,0,0,1-.28.52,2.69,2.69,0,0,1-.37.45,2.82,2.82,0,0,1-.46.38,3.1,3.1,0,0,1-.51.27,2.26,2.26,0,0,1-.56.17A3,3,0,0,1,15.12,22.32Z" />
+                                        <path class="cls-1" d="M45.68,34.43H22.6a3,3,0,0,1,0-6H45.68a3,3,0,0,1,0,6Z" />
+                                        <path class="cls-1" d="M15.12,34.43A3,3,0,0,1,13,29.31a3.23,3.23,0,0,1,.45-.38,3.67,3.67,0,0,1,.52-.27,2.26,2.26,0,0,1,.56-.17,3,3,0,1,1,.59,5.94Z" />
+                                    </g>
+                                </g>
+                            </svg></div>
+                        <div class="t-x list-group-text">我的訂單</div>
+
+                    </a>
+                    <a class="col-4 col-lg-12 list-group-item list-group-item-action d-flex align-items-center justify-content-center" href=" account-wishlist.php">
+                        <div class="account-icon mr-2 list-group-icon ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 62.64 65.01">
+                                <defs>
+                                    <style>
+                                        .cls-1 {
+                                            fill: #3c2e26;
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="圖層_2" data-name="圖層 2">
+                                    <g id="圖層_1-2" data-name="圖層 1">
+                                        <g id="圖層_2-2" data-name="圖層 2">
+                                            <g id="圖層_1-2-2" data-name="圖層 1-2">
+                                                <path class="cls-1" d="M31.31,65A3.51,3.51,0,0,1,29,64.1C27.9,63.14,3.36,40.5.37,20.87a16.42,16.42,0,0,1-.24-5.53A12.94,12.94,0,0,1,1.78,9.71,17.34,17.34,0,0,1,31.32,7.08,17.32,17.32,0,0,1,60.89,9.79a12.87,12.87,0,0,1,1.6,5.38,15.72,15.72,0,0,1,.15,2.17,17.88,17.88,0,0,1-.4,3.64C59.17,40.59,34.71,63.14,33.67,64.1A3.54,3.54,0,0,1,31.31,65Zm-14-58A10.38,10.38,0,0,0,8,12.9a1.9,1.9,0,0,1-.2.37A6.08,6.08,0,0,0,7.09,16a.33.33,0,0,1,0,.14c0,.41-.08.83-.09,1.25a10.59,10.59,0,0,0,.23,2.17v.22c2.07,13.93,18,30.87,24,36.92,6.08-6,21.9-22.92,24-36.84v-.23a10,10,0,0,0,.26-2.24,11.9,11.9,0,0,0-.1-1.34v-.15a6,6,0,0,0-.69-2.56c-.07-.11-.13-.23-.19-.34A10.31,10.31,0,0,0,35,17a3.52,3.52,0,0,1-1.56,2.79l-.16.11a3.52,3.52,0,0,1-3.93,0l-.15-.11A3.52,3.52,0,0,1,27.64,17,10.28,10.28,0,0,0,17.33,7Z" />
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg></div>
+                        <div class="t-x list-group-text">願望清單</div>
+
+                    </a>
+                    <a class="col-4 col-lg-12 list-group-item list-group-item-action d-flex align-items-center justify-content-center" href=" account-mailbox.php">
+                        <div class="account-icon mr-2 list-group-icon ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67.43 47.57">
+                                <defs>
+                                    <style>
+                                        .cls-1 {
+                                            fill: #3c2e26;
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="圖層_2" data-name="圖層 2">
+                                    <g id="圖層_1-2" data-name="圖層 1">
+                                        <path class="cls-1" d="M63.93,0H3.5A3.5,3.5,0,0,0,0,3.5V44.07a3.5,3.5,0,0,0,3.5,3.5H63.93a3.5,3.5,0,0,0,3.5-3.5V3.5A3.5,3.5,0,0,0,63.93,0ZM53.5,7,33.71,21.79,13.93,7ZM7,40.57v-30L31.62,29a3.49,3.49,0,0,0,4.19,0L60.43,10.56v30Z" />
+                                    </g>
+                                </g>
+                            </svg></div>
+                        <div class="t-x list-group-text">會員信箱</div>
+
+                    </a>
+                    <a class="col-4 col-lg-12 list-group-item list-group-item-action d-flex align-items-center justify-content-center" href=" account-coupon.php">
+                        <div class="account-icon mr-2 list-group-icon ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 77.4 51.07">
+                                <defs>
+                                    <style>
+                                        .cls-1 {
+                                            fill: #3c2e26;
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="圖層_2" data-name="圖層 2">
+                                    <g id="圖層_1-2" data-name="圖層 1">
+                                        <path class="cls-1" d="M74.4,51.07H3a3,3,0,0,1-3-3V33.51a3,3,0,0,1,2.08-2.86,5.33,5.33,0,0,0,0-10.23A3,3,0,0,1,0,17.56V3A3,3,0,0,1,3,0H74.4a3,3,0,0,1,3,3V17.56a3,3,0,0,1-2.08,2.86,5.33,5.33,0,0,0,0,10.23,3,3,0,0,1,2.08,2.86V48.07A3,3,0,0,1,74.4,51.07ZM6,45.07H71.4v-9.6a11.31,11.31,0,0,1,0-19.87V6H6v9.6A11.31,11.31,0,0,1,6,35.47Z" />
+                                        <g id="圖層_2-2" data-name="圖層 2">
+                                            <g id="圖層_1-2-2" data-name="圖層 1-2">
+                                                <path class="cls-1" d="M46.62,25.65a14.16,14.16,0,0,0-17.41,0,8.91,8.91,0,0,0-3.79,6.73c0,4.42,4.79,7.18,12.48,7.18s12.52-2.76,12.52-7.18A8.92,8.92,0,0,0,46.62,25.65Z" />
+                                                <path class="cls-1" d="M38,20.82a5,5,0,0,0,5.21-4.66A5,5,0,0,0,38,11.51a5,5,0,0,0-5.22,4.65A5,5,0,0,0,38,20.82Z" />
+                                                <path class="cls-1" d="M54.68,19.39a5.75,5.75,0,0,0-6.22-3.6,4.24,4.24,0,0,0-3.31,5.78,5.53,5.53,0,0,0,2.5,2.89,5.84,5.84,0,0,0,2.85.78,5.26,5.26,0,0,0,.89-.08,4.22,4.22,0,0,0,3.29-5.77Z" />
+                                                <path class="cls-1" d="M30.71,21.57a4.36,4.36,0,0,0-.26-3.7,4.42,4.42,0,0,0-3.07-2.08,5.65,5.65,0,0,0-3.75.73c-2.5,1.44-3.5,4.39-2.22,6.59h0a4.38,4.38,0,0,0,3.07,2.07,4.49,4.49,0,0,0,.89.06,5.81,5.81,0,0,0,2.85-.78A5.55,5.55,0,0,0,30.71,21.57Z" />
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="t-x list-group-text">優惠券</div>
+
+                    </a>
+                    <a class="col-4 col-lg-12 list-group-item list-group-item-action d-flex align-items-center justify-content-center" href=" account-qa.php">
+                        <div class="account-icon mr-2 list-group-icon ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76.45 71.11">
+                                <defs>
+                                    <style>
+                                        .cls-2 {
+                                            stroke: #fff;
+                                            stroke-miterlimit: 10;
+                                            stroke-width: 3px;
+                                        }
+                                    </style>
+                                </defs>
+                                <g id="圖層_2" data-name="圖層 2">
+                                    <g id="圖層_1-2" data-name="圖層 1">
+                                        <g id="圖層_2-2" data-name="圖層 2">
+                                            <g id="圖層_1-2-2" data-name="圖層 1-2">
+                                                <path class="cls-1" d="M43.79,34.08a16,16,0,1,0-22.63-.23l.63.61C9.1,39.44,0,52.41,0,67.6a3.5,3.5,0,0,0,3.5,3.5H62.33a2.94,2.94,0,0,0,.42,0,2.94,2.94,0,0,0,.42,0,3.5,3.5,0,0,0,3.5-3.5C66.67,52,57.07,38.77,43.79,34.08ZM32.6,13.77a9,9,0,1,1-9,9A9,9,0,0,1,32.6,13.77ZM7.2,64.1c1.61-14,12.72-24.81,26.14-24.81S57.86,50.14,59.47,64.1Z" />
+                                            </g>
+                                        </g>
+                                        <path class="cls-2" d="M51.5,27.42l.18-.5a6.14,6.14,0,0,1,2.63-4,8,8,0,0,1,3.93-1.27,10,10,0,0,0,3.84-.88,4.05,4.05,0,0,0,2.37-2.91,4.41,4.41,0,0,0-.39-3.51,5.06,5.06,0,0,0-3.26-2,6.69,6.69,0,0,0-3.66-.12,3.24,3.24,0,0,0-1.74,1.17l-.38.72a7.69,7.69,0,0,1-2,2.19,2.59,2.59,0,0,1-2.19.38,9.89,9.89,0,0,1-3-1.55c-2.31-1.61-3.29-3.13-2.93-4.58a5.26,5.26,0,0,1,.86-1.85l1-1.51a15.24,15.24,0,0,1,1.94-2A14.82,14.82,0,0,1,52,3a14.87,14.87,0,0,1,4.62-1.31A16,16,0,0,1,62.73,2a15.29,15.29,0,0,1,10,7.05A14.36,14.36,0,0,1,74.52,20.2,13.25,13.25,0,0,1,72.28,25,11.25,11.25,0,0,1,69,28.12a16.39,16.39,0,0,1-6.11,2.29l-1.1.07a13.07,13.07,0,0,1-.5,1.64,3.37,3.37,0,0,1-.88,1.1,2.78,2.78,0,0,1-1.83.57,12.7,12.7,0,0,1-3-.44,11.7,11.7,0,0,1-2.9-1.05,2.52,2.52,0,0,1-1.29-1.47,3.91,3.91,0,0,1-.21-1.5A12.9,12.9,0,0,1,51.5,27.42Zm-3,11.34a3.11,3.11,0,0,1,2.17-1,14.45,14.45,0,0,1,4.23.57,5.73,5.73,0,0,1,3.75,2.23c.62,1,.65,2.75.06,5.13a8.92,8.92,0,0,1-1.57,3.64,3.31,3.31,0,0,1-2.35,1.19,15,15,0,0,1-4.16-.58,6.27,6.27,0,0,1-3.89-2.37,5.32,5.32,0,0,1-.17-4l.25-1a20.16,20.16,0,0,1,.64-2.21A5.4,5.4,0,0,1,48.45,38.76Z" />
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="t-x list-group-text">Q&A</div>
+
+                    </a>
                     <!-- </div> -->
 
 
@@ -41,6 +178,9 @@
                     // console.log('check if item?', item, window.location.pathname.indexOf(item))
                     if (window.location.pathname.indexOf(item) != -1) {
                         $('.list-group-item').removeClass('active').eq(index).addClass('active')
+                        $('.list-group-text').removeClass('active').eq(index).addClass('active')
+                        $('.account-icon-or').removeClass('active').eq(index).addClass('active')
+
                     }
 
                 })
