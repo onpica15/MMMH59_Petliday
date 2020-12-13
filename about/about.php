9e8b08c096c43f859ef4d843630e71f7d9eb8bfb
YuChieh <la0603c@gmail.com>
2020-12-14 00:23:04 +0800

    about-pc&m-OK

--- about/about.php
+++ about/about.php
@@ -5,36 +5,72 @@
 
 <?php include __DIR__ . '/../parts/html-navbar.php' ?>
 <!-- ------------------ body開始 以上勿刪 ------------------ -->
-<!-- 照片的部分始 -->
-<hero>
-    <div class="hero mt-5">
-        <img src="./img/about_02.png">
-    </div>
-</hero>
 
-<section1>
-    <div class="section1">
-        <img src="./img/about_03.png">
-    </div>
-</section1>
+<!-- 電腦版 -->
+<div class="computer-imgs">
+    <hero>
+        <div class="hero">
+            <img src="./img/about_hero.png">
+        </div>
+    </hero>
 
-<section2>
-    <div class="section1">
-        <img src="./img/about_04.png">
-    </div>
-</section2>
+    <section1>
+        <div class="section1">
+            <img src="./img/about_sec1.png">
+        </div>
+    </section1>
 
-<section3>
-    <div class="section1">
-        <img src="./img/about_05.png">
-    </div>
-</section3>
+    <section2>
+        <div class="section2">
+            <img src="./img/about_sec2.png">
+        </div>
+    </section2>
 
-<section4>
-    <div class="section1">
-        <img src="./img/about_06.png">
-    </div>
-</section4>
+    <section3>
+        <div class="section3">
+            <img src="./img/about_sec3.png">
+        </div>
+    </section3>
+
+    <section4>
+        <div class="section4">
+            <img src="./img/about_sec4.png">
+        </div>
+    </section4>
+</div>
+
+<!-- 手機版 -->
+<div class="phone-imgs">
+    <hero>
+        <div class="hero d-block d-sm-none">
+            <img src="./img/about_m_hero.png">
+        </div>
+    </hero>
+
+    <section1>
+        <div class="section1 d-block d-sm-none">
+            <img src="./img/about_m_sec1.png">
+        </div>
+    </section1>
+
+    <section2>
+        <div class="section2 d-block d-sm-none">
+            <img src="./img/about_m_sec2.png">
+        </div>
+    </section2>
+
+    <section3>
+        <div class="section3 d-block d-sm-none">
+            <img src="./img/about_m_sec3.png">
+        </div>
+    </section3>
+
+    <section4>
+        <div class="section4 d-block d-sm-none">
+            <img src="./img/about_m_sec4.png">
+        </div>
+    </section4>
+</div>
 
 <!-- Petliday做了什麼的部分末 -->
 <?php include __DIR__ . '/../userlogin/user-login.php' ?>
